a9ba5b8fcb336f0d030aed4588dfc07666a34285
E.Walter <e.walter@physik.uni-muenchen.de>
2019-07-31 10:26:33 +0200

    minor edit in util.h

--- Keldysh_mfRG/util.h
+++ Keldysh_mfRG/util.h
@@ -8,6 +8,7 @@
 #include <sys/time.h>  // system time
 #include <unistd.h>    // time delay
 #include <string>
+#include <iostream>
 
 /* return time stamp in seconds with millisecond precision */
 double get_time() {
@@ -24,28 +25,28 @@ void get_time(double t0) {
     gettimeofday(&tp, NULL);
     long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
     double t = ms/1000.;
-    cout << "time elapsed: ";
+    std::cout << "time elapsed: ";
     printf("%.3f", t-t0);
-    cout << "s" << endl;
+    std::cout << "s" << std::endl;
 }
 
 /* display time difference in seconds w.r.t. reference time, with microsecond precision */
-void get_time(double t0, string prec) {
+void get_time(double t0, std::string prec) {
     struct timeval tp;
     gettimeofday(&tp, NULL);
     if (prec == "us") {
         long int us = tp.tv_sec * 1000000 + tp.tv_usec;
         double t = us / 1000000.;
-        cout << "time elapsed: ";
+        std::cout << "time elapsed: ";
         printf("%.6f", t-t0);
-        cout << "s" << endl;
+        std::cout << "s" << std::endl;
     }
     else {
         long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
         double t = ms / 1000.;
-        cout << "time elapsed: ";
+        std::cout << "time elapsed: ";
         printf("%.3f", t-t0);
-        cout << "s" << endl;
+        std::cout << "s" << std::endl;
     }
 }
 
