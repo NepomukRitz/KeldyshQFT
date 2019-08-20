f255342cedf0c57fd7401e164aa5a754a77525aa
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-08-20 14:39:14 +0200

    Implemented (correctly) the internal symmetries of the Keldysh vertex for all diagrammatic classes

--- Keldysh_mfRG/Keldysh_symmetries.h
+++ Keldysh_mfRG/Keldysh_symmetries.h
@@ -51,58 +51,219 @@ int T_3_Keldysh(int iK) {
 
 /*The next functions are the group of actions that transform related Keldysh components within the vertex into one another.
  * In lack of a better nomenclature, name them A, since they switch alphas...*/
-int A1(int iK)
+
+/*T_1 switches the incoming legs*/
+int T_1_Keldysh(int iK)
+{
+    int alpha1p, alpha2p, alpha1, alpha2, iKp;
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    iKp = alpha1p*8 + alpha2p*4 + alpha2*2 + alpha1;
+
+    return iKp;
+}
+
+/* T2 switches the outgoing legs */
+int T_2_Keldysh(int iK)
+{
+    int alpha1p, alpha2p, alpha1, alpha2, iKp;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    iKp = alpha2p*8 + alpha1p*4 + alpha1*2 + alpha2;
+
+    return iKp;
+}
+
+/* T3 switches both incoming and outgoing legs */
+int T_3_Keldysh(int iK) {
+    int alpha1p, alpha2p, alpha1, alpha2, iKp;
+
+    alpha2 = iK % 2;
+    alpha1 = (iK % 4) / 2;
+    alpha2p = (iK % 8) / 4;
+    alpha1p = (iK % 16) / 8;
+
+    iKp = alpha2p * 8 + alpha1p * 4 + alpha2 * 2 + alpha1;
+
+    return iKp;
+}
+
+
+/*The next functions are the group of actions that transform related Keldysh components within the vertex into one another.
+ * In lack of a better nomenclature, name them A, since they switch alphas...*/
+int A1_K1a(int iK)
 {
     int alpha1p,alpha2p, alpha1, alpha2;
     int index0, index1, index2, index3;
 
-    alpha2 = iK%2-1;
-    alpha1 = (iK%4)/2-1;
-    alpha2p = (iK%8)/4-1;
-    alpha1p = (iK%16)/8-1;
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
 
     index3 = (alpha1p+1)%2;
     index2 = (alpha2p  )%2;
     index1 = (alpha1   )%2;
-    index0 = (alpha2+1) %2;
+    index0 = (alpha2+1 )%2;
 
-    return (index3+1)*8 + (index2+1)*4 + (index1+1)*2 + (index0+1);
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
 }
 
-int A2(int iK)
+int A2_K1a(int iK)
 {
     int alpha1p,alpha2p, alpha1, alpha2;
     int index0, index1, index2, index3;
 
-    alpha2 = iK%2-1;
-    alpha1 = (iK%4)/2-1;
-    alpha2p = (iK%8)/4-1;
-    alpha1p = (iK%16)/8-1;
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
 
     index3 = (alpha1p  )%2;
     index2 = (alpha2p+1)%2;
     index1 = (alpha1 +1)%2;
     index0 = (alpha2   )%2;
 
-    return (index3+1)*8 + (index2+1)*4 + (index1+1)*2 + (index0+1);
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
 }
 
-int A3(int iK)
+int A3_K1a(int iK)
 {
     int alpha1p,alpha2p, alpha1, alpha2;
     int index0, index1, index2, index3;
 
-    alpha2 = iK%2-1;
-    alpha1 = (iK%4)/2-1;
-    alpha2p = (iK%8)/4-1;
-    alpha1p = (iK%16)/8-1;
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
 
     index3 = (alpha1p+1)%2;
     index2 = (alpha2p+1)%2;
     index1 = (alpha1 +1)%2;
     index0 = (alpha2 +1)%2;
 
-    return (index3+1)*8 + (index2+1)*4 + (index1+1)*2 + (index0+1);
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+
+
+int A1_K1p(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p+1)%2;
+    index2 = (alpha2p+1)%2;
+    index1 = (alpha1   )%2;
+    index0 = (alpha2   )%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+int A2_K1p(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p  )%2;
+    index2 = (alpha2p  )%2;
+    index1 = (alpha1 +1)%2;
+    index0 = (alpha2 +1)%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+int A3_K1p(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p+1)%2;
+    index2 = (alpha2p+1)%2;
+    index1 = (alpha1 +1)%2;
+    index0 = (alpha2 +1)%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+
+
+
+
+int A1_K1t(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p  )%2;
+    index2 = (alpha2p+1)%2;
+    index1 = (alpha1   )%2;
+    index0 = (alpha2 +1)%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+int A2_K1t(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p+1)%2;
+    index2 = (alpha2p  )%2;
+    index1 = (alpha1 +1)%2;
+    index0 = (alpha2   )%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+}
+
+int A3_K1t(int iK)
+{
+    int alpha1p,alpha2p, alpha1, alpha2;
+    int index0, index1, index2, index3;
+
+    alpha2 = iK%2;
+    alpha1 = (iK%4)/2;
+    alpha2p = (iK%8)/4;
+    alpha1p = (iK%16)/8;
+
+    index3 = (alpha1p+1)%2;
+    index2 = (alpha2p+1)%2;
+    index1 = (alpha1 +1)%2;
+    index0 = (alpha2 +1)%2;
+
+    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
 }
 
 
