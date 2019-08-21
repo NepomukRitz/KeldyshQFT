408ecdf547c5d675a6c9dcd781e297a965341dde
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-08-21 16:30:13 +0200

    Implemented functions to calculate Keldysh indices of non-zero elements in a vertex derivative

--- Keldysh_mfRG/Keldysh_symmetries.h
+++ Keldysh_mfRG/Keldysh_symmetries.h
@@ -51,175 +51,203 @@ int T_3_Keldysh(int iK) {
 }
 
 
-/*The next functions are the group of actions that transform related Keldysh components within the vertex into one another.
- * In lack of a better nomenclature, name them A, since they switch alphas...*/
-int A1_K1a(int iK)
+//*The next functions are the group of actions that transform related Keldysh components within the vertex into one another.
+// * In lack of a better nomenclature, name them A, since they switch alphas...*/
+//int A1_K1a(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p  )%2;
+//    index1 = (alpha1   )%2;
+//    index0 = (alpha2+1 )%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A2_K1a(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p  )%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2   )%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A3_K1a(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2 +1)%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//
+//
+//int A1_K1p(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1   )%2;
+//    index0 = (alpha2   )%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A2_K1p(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p  )%2;
+//    index2 = (alpha2p  )%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2 +1)%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A3_K1p(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2 +1)%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//
+//
+//
+//
+//int A1_K1t(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p  )%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1   )%2;
+//    index0 = (alpha2 +1)%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A2_K1t(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p  )%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2   )%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+//
+//int A3_K1t(int iK)
+//{
+//    int alpha1p,alpha2p, alpha1, alpha2;
+//    int index0, index1, index2, index3;
+//
+//    alpha2 = iK%2;
+//    alpha1 = (iK%4)/2;
+//    alpha2p = (iK%8)/4;
+//    alpha1p = (iK%16)/8;
+//
+//    index3 = (alpha1p+1)%2;
+//    index2 = (alpha2p+1)%2;
+//    index1 = (alpha1 +1)%2;
+//    index0 = (alpha2 +1)%2;
+//
+//    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+//}
+
+/* This function converts indices in the range 0...5 to the actual Keldysh inndex they correspond to
+ * Rule: {0,1,2,3,4,5} -> {0,1,3,5,6,7}*/
+int convertToRealIndex(int index)
 {
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p  )%2;
-    index1 = (alpha1   )%2;
-    index0 = (alpha2+1 )%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+    if(index ==0 || index == 1)
+        return index;
+    else if(index == 2)
+        return 3;
+    else
+        return index + 2;
 }
 
-int A2_K1a(int iK)
+/*This function returns the values of the 4 alphas for a given index in the 0...15 set */
+tuple<int, int, int, int> alphas(int index)
 {
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
+    int alpha1p, alpha2p, alpha1, alpha2;
 
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
+    alpha2 = index % 2+1;
+    alpha1 = (index % 4) / 2+1;
+    alpha2p = (index % 8) / 4+1;
+    alpha1p = (index % 16) / 8+1;
 
-    index3 = (alpha1p  )%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2   )%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-int A3_K1a(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2 +1)%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
+    return make_tuple(alpha1p, alpha2p, alpha1, alpha2);
 }
 
 
 
-int A1_K1p(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1   )%2;
-    index0 = (alpha2   )%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-int A2_K1p(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p  )%2;
-    index2 = (alpha2p  )%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2 +1)%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-int A3_K1p(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2 +1)%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-
-
-
-
-int A1_K1t(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p  )%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1   )%2;
-    index0 = (alpha2 +1)%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-int A2_K1t(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p  )%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2   )%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
-
-int A3_K1t(int iK)
-{
-    int alpha1p,alpha2p, alpha1, alpha2;
-    int index0, index1, index2, index3;
-
-    alpha2 = iK%2;
-    alpha1 = (iK%4)/2;
-    alpha2p = (iK%8)/4;
-    alpha1p = (iK%16)/8;
-
-    index3 = (alpha1p+1)%2;
-    index2 = (alpha2p+1)%2;
-    index1 = (alpha1 +1)%2;
-    index0 = (alpha2 +1)%2;
-
-    return (index3)*8 + (index2)*4 + (index1)*2 + (index0);
-}
 
 
 #endif //KELDYSH_MFRG_KELDYSH_SYMMETRIES_H
