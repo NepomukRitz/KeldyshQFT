9ae59f1db9a7bf2c158016983e4573cbaa8e0180
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-09-03 09:38:03 +0200

    .

--- Keldysh_mfRG/susceptibility.h
+++ Keldysh_mfRG/susceptibility.h
@@ -9,14 +9,12 @@
 #include "parameters.h"
 #include "frequency_grid.h"
 
-#define SUSC 1
-
 #ifdef SUSC
 
 /******************CLASS FOR SELF ENERGY *************/
 template <typename Q>
 class Susc{
-    vec<Q> Susceptibility =  vec<Q> (2*nSUSC); // factor 2 for Keldysh components: Susc^R, Susc^K TODO: check if the susceptibility also has this structure or if one needs all three components!
+    vec<Q> Susceptibility =  vec<Q> (2*nSUSC); // factor 2 for Keldysh components: Susc^R, Susc^K. Susc^A = [Sus^R] dagger
 public:
     void setsus(int, int, Q);
     Q susval(int, int);
@@ -30,13 +28,10 @@ public:
 
 
 /*****************************************FUNCTIONS FOR SELF ENERGY********************************************************/
-template <typename Q>
-Q Susc<Q>::susval(int iK, int i){
+template <typename Q>Q Susc<Q>::susval(int iK, int i){
     return Susceptibility[iK*nSE + i];
 }
-
-template <typename Q>
-Q Susc<Q>::susvalsmooth(int iK, double w){//smoothly interpolates for values between discrete frequency values of mesh
+template <typename Q>Q Susc<Q>::susvalsmooth(int iK, double w){//smoothly interpolates for values between discrete frequency values of mesh
 
     int W = fconv(w);
 
@@ -49,33 +44,27 @@ Q Susc<Q>::susvalsmooth(int iK, double w){//smoothly interpolates for values bet
 
     return (1.-xd)*f1 + xd*f2;
 }
-
-template <typename Q>
-void Susc<Q>::setsus(int iK, int i, Q val){
+template <typename Q>void Susc<Q>::setsus(int iK, int i, Q val){
     Susceptibility[iK*nSE + i] = val;
 }
 
 //operators for susceptibility
-template <typename Q>
-Susc<Q> operator*(Q alpha, const Susc<Q>& sus1){//product operator overloading
+template <typename Q>Susc<Q> operator*(Q alpha, const Susc<Q>& sus1){//product operator overloading
     Susc<Q> sus2;
     sus2.Sigma = sus1.Sigma * alpha;
     return sus2;
 }
-template <typename Q>
-Susc<Q> operator*(const Susc<Q>& sus1, Q alpha){//product operator overloading
+template <typename Q>Susc<Q> operator*(const Susc<Q>& sus1, Q alpha){//product operator overloading
     Susc<Q> sus2;
     sus2.Sigma = sus1.Sigma * alpha;
     return sus2;
 }
-template <typename Q>
-Susc<Q> operator+(const Susc<Q>& sus1, const Susc<Q>& sus2){//sum operator overloading
+template <typename Q>Susc<Q> operator+(const Susc<Q>& sus1, const Susc<Q>& sus2){//sum operator overloading
     Susc<Q> sus3;
     sus3.Sigma = sus1.Sigma + sus2.Sigma;
     return sus3;
 }
-template <typename Q>
-Susc<Q> operator+=(const Susc<Q>& sus1, const Susc<Q>& sus2){//sum operator overloading
+template <typename Q>Susc<Q> operator+=(const Susc<Q>& sus1, const Susc<Q>& sus2){//sum operator overloading
     Susc<Q> sus3;
     sus3.Sigma = sus1.Sigma + sus2.Sigma;
     return sus3;
