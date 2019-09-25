404d80ffa3a66507071906726cb7bf703cd2c7f0
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-09-25 16:02:41 +0200

    .

--- Keldysh_mfRG/selfenergy.h
+++ Keldysh_mfRG/selfenergy.h
@@ -9,22 +9,35 @@
 /******************CLASS FOR SELF ENERGY *************/
 template <typename Q>
 class SelfEnergy{
-public:
     vec<Q> Sigma =  vec<Q> (2*nSE); // factor 2 for Keldysh components: Sigma^R, Sigma^K
-
+public:
     Q sval(int, int);
     Q svalsmooth(int, double);
     void setself(int, int, Q);
 
-//    SelfEnergy<Q> operator+(const SelfEnergy<Q>& self1, const SelfEnergy<Q>& self2);
-//    SelfEnergy<Q> operator+=(const SelfEnergy<Q>& self1,const SelfEnergy<Q>& self2);
-//    SelfEnergy<Q> operator*(Q alpha, const SelfEnergy<Q>& self1);
-//    SelfEnergy<Q> operator*(const SelfEnergy<Q>& self1, Q alpha);
+//operators for self energy
+
+    SelfEnergy<Q> operator+(const SelfEnergy<Q>& self1){//sum operator overloading
+        this->Sigma + self1.Sigma;
+        return *this;
+    }
+    SelfEnergy<Q> operator+=(const SelfEnergy<Q>& self1){//sum operator overloading
+        this->Sigma += self1.Sigma;
+        return *this;
+    }
+    SelfEnergy<Q> operator*(Q alpha) {//multiplication operator overloading
+        this->Sigma * alpha;
+        return *this;
+    }
+    SelfEnergy<Q> operator*(double alpha){//multiplication operator overloading
+        this->Sigma*alpha;
+        return *this;
+    }
 };
 
 
 
-/*****************************************FUNCTIONS FOR SELF ENERGY********************************************************/
+/*****************************************FUNCTIONS FOR SELF ENERGY*****************************************************/
 template <typename Q> Q SelfEnergy<Q>::sval(int iK, int i){
     return Sigma[iK*nSE + i];
 }
@@ -42,38 +55,6 @@ template <typename Q> Q SelfEnergy<Q>::svalsmooth(int iK, double w){//smoothly i
 }
 template <typename Q> void SelfEnergy<Q>::setself(int iK, int i, Q val){
     Sigma[iK*nSE + i] = val;
-}
-
-//operators for self energy
-template <typename Q> SelfEnergy<Q> operator+(const SelfEnergy<Q>& self1, const SelfEnergy<Q>& self2){//sum operator overloading
-    SelfEnergy<Q> self3;
-    self3.Sigma = self1.Sigma + self2.Sigma;
-    return self3;
-}
-template <typename Q> SelfEnergy<Q> operator+=(const SelfEnergy<Q>& self1, const SelfEnergy<Q>& self2){//sum operator overloading
-    SelfEnergy<Q> self3;
-    self3.Sigma = self1.Sigma + self2.Sigma;
-    return self3;
-}
-template <typename Q> SelfEnergy<Q> operator*(Q alpha, const SelfEnergy<Q>& self1){//product operator overloading
-    SelfEnergy<Q> self2;
-    self2.Sigma = self1.Sigma * alpha;
-    return self2;
-}
-template <typename Q> SelfEnergy<Q> operator*(const SelfEnergy<Q>& self1, Q alpha){//product operator overloading
-    SelfEnergy<Q> self2;
-    self2.Sigma = self1.Sigma * alpha;
-    return self2;
-}
-template <typename Q> SelfEnergy<Q> operator*(double alpha, const SelfEnergy<Q>& self1){//product operator overloading
-    SelfEnergy<Q> self2;
-    self2.Sigma = self1.Sigma * alpha;
-    return self2;
-}
-template <typename Q> SelfEnergy<Q> operator*(const SelfEnergy<Q>& self1, double alpha){//product operator overloading
-    SelfEnergy<Q> self2;
-    self2.Sigma = self1.Sigma * alpha;
-    return self2;
 }
 
 
