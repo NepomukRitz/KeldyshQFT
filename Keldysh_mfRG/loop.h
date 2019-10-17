a52685d98437f474c336cbfd44ba3c0f82b3facd
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-10-17 16:58:13 +0200

    Loop now partially supports internal structure

--- Keldysh_mfRG/loop.h
+++ Keldysh_mfRG/loop.h
@@ -17,27 +17,28 @@ class IntegrandR{
     Vertex<T>& vertex;
     Propagator& propagator;
     double w;
+    int i_in;
 
 public:
-    IntegrandR(Vertex<T>& vertex_in, Propagator& prop_in, double w_in)
-        : vertex(vertex_in), propagator(prop_in), w(w_in) {};
+    IntegrandR(Vertex<T>& vertex_in, Propagator& prop_in, double w_in, int i_in_in)
+        : vertex(vertex_in), propagator(prop_in), w(w_in), i_in(i_in_in) {};
 
     Q operator()(double wp)
     {
         return
-        -( (vertex.densvertex.avertex.value(3, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(3, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(3, 0., wp, w, 0, vertex.densvertex.avertex) +
+        -( (vertex.densvertex.avertex.value(3, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(3, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(3, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(3) ) * propagator.pvalsmooth(0, wp) +
 
-           (vertex.densvertex.avertex.value(6, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(6, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(6, 0., wp, w, 0, vertex.densvertex.avertex) +
+           (vertex.densvertex.avertex.value(6, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(6, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(6, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(6) ) * conj(propagator.pvalsmooth(0, wp)) +
 
-           (vertex.densvertex.avertex.value(7, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(7, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(7, 0., wp, w, 0, vertex.densvertex.avertex) +
+           (vertex.densvertex.avertex.value(7, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(7, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(7, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(7) ) * propagator.pvalsmooth(1, wp) );
     }
 };
@@ -48,27 +49,28 @@ class IntegrandK{
     Vertex<T>& vertex;
     Propagator& propagator;
     double w;
+    int i_in;
 
 public:
-    IntegrandK(Vertex<T>& vertex_in, Propagator& prop_in, double w_in)
-            : vertex(vertex_in), propagator(prop_in), w(w_in) {};
+    IntegrandK(Vertex<T>& vertex_in, Propagator& prop_in, double w_in, int i_in_in)
+            : vertex(vertex_in), propagator(prop_in), w(w_in), i_in(i_in_in) {};
 
     Q operator()(double wp)
     {
         return
-        -( (vertex.densvertex.avertex.value(1, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(1, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(1, 0., wp, w, 0, vertex.densvertex.avertex) +
+        -( (vertex.densvertex.avertex.value(1, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(1, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(1, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(1) ) * propagator.pvalsmooth(0, wp) +
 
-           (vertex.densvertex.avertex.value(4, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(4, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(4, 0., wp, w, 0, vertex.densvertex.avertex) +
+           (vertex.densvertex.avertex.value(4, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(4, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(4, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(4) ) * conj(propagator.pvalsmooth(0, wp)) +
 
-           (vertex.densvertex.avertex.value(5, wp-w, 0.5*(w+wp), 0.5*(w+wp), 0, vertex.densvertex.tvertex) +
-            vertex.densvertex.pvertex.value(5, wp+w, 0.5*(w-wp), 0.5*(w-wp), 0) +
-            vertex.densvertex.tvertex.value(5, 0., wp, w, 0, vertex.densvertex.avertex) +
+           (vertex.densvertex.avertex.value(5, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) +
+            vertex.densvertex.pvertex.value(5, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) +
+            vertex.densvertex.tvertex.value(5, 0., wp, w, i_in, vertex.densvertex.avertex) +
             vertex.densvertex.irred.vval(5) ) * propagator.pvalsmooth(1, wp) );
     }
 };
@@ -77,13 +79,16 @@ public:
 SelfEnergy<comp> loop(Vertex<fullvert<comp> >& fullvertex, Propagator& prop)
 {
     SelfEnergy<comp> resp = SelfEnergy<comp> ();
-
+//TODO complete support for internal structure
 #pragma omp parallel for
-    for (int i=0; i<nSE; ++i){
+    for (int iSE=0; iSE<nSE*n_in; ++iSE){
+        int i = ( (iSE%(nSE*n_in))/n_in);
+        int i_in = iSE%n_in;
+
         double w = ffreqs[i];
 
-        IntegrandR<comp, fullvert<comp> > integrandR(fullvertex, prop, w);
-        IntegrandK<comp, fullvert<comp> > integrandK(fullvertex, prop, w);
+        IntegrandR<comp, fullvert<comp> > integrandR(fullvertex, prop, w, i_in);
+        IntegrandK<comp, fullvert<comp> > integrandK(fullvertex, prop, w, i_in);
 
         comp integratedR = integrator(integrandR, ffreqs);
         comp integratedK = integrator(integrandK, ffreqs);
