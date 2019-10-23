7ba2b405c030368bdfb6d9e113e8792ddd1e4a6e
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-10-23 10:45:04 +0200

    Included file with trivial internal symmetries

---
+++ Keldysh_mfRG/internal_symmetries.h
@@ -0,0 +1,126 @@
+//
+// Created by Sa.Aguirre on 10/23/19.
+//
+
+#ifndef KELDYSH_MFRG_INTERNAL_SYMMETRIES_H
+#define KELDYSH_MFRG_INTERNAL_SYMMETRIES_H
+
+int internal_T1_K1_a(int i_in){
+    return i_in;
+}
+int internal_T1_K2_a(int i_in){
+    return i_in;
+}
+int internal_T1_K3_a(int i_in){
+    return i_in;
+}
+int internal_T2_K1_a(int i_in){
+    return i_in;
+}
+int internal_T2_K2_a(int i_in){
+    return i_in;
+}
+int internal_T2_K3_a(int i_in){
+    return i_in;
+}
+int internal_T3_K1_a(int i_in){
+    return i_in;
+}
+int internal_T3_K2_a(int i_in){
+    return i_in;
+}
+int internal_T3_K3_a(int i_in){
+    return i_in;
+}
+int internal_TC_K1_a(int i_in){
+    return i_in;
+}
+int internal_TC_K2_a(int i_in){
+    return i_in;
+}
+int internal_TC_K3_a(int i_in){
+    return i_in;
+}
+
+
+
+
+int internal_T1_K1_p(int i_in){
+    return i_in;
+}
+int internal_T1_K2_p(int i_in){
+    return i_in;
+}
+int internal_T1_K3_p(int i_in){
+    return i_in;
+}
+int internal_T2_K1_p(int i_in){
+    return i_in;
+}
+int internal_T2_K2_p(int i_in){
+    return i_in;
+}
+int internal_T2_K3_p(int i_in){
+    return i_in;
+}
+int internal_T3_K1_p(int i_in){
+    return i_in;
+}
+int internal_T3_K2_p(int i_in){
+    return i_in;
+}
+int internal_T3_K3_p(int i_in){
+    return i_in;
+}
+int internal_TC_K1_p(int i_in){
+    return i_in;
+}
+int internal_TC_K2_p(int i_in){
+    return i_in;
+}
+int internal_TC_K3_p(int i_in){
+    return i_in;
+}
+
+
+
+int internal_T1_K1_t(int i_in){
+    return i_in;
+}
+int internal_T1_K2_t(int i_in){
+    return i_in;
+}
+int internal_T1_K3_t(int i_in){
+    return i_in;
+}
+int internal_T2_K1_t(int i_in){
+    return i_in;
+}
+int internal_T2_K2_t(int i_in){
+    return i_in;
+}
+int internal_T2_K3_t(int i_in){
+    return i_in;
+}
+int internal_T3_K1_t(int i_in){
+    return i_in;
+}
+int internal_T3_K2_t(int i_in){
+    return i_in;
+}
+int internal_T3_K3_t(int i_in){
+    return i_in;
+}
+int internal_TC_K1_t(int i_in){
+    return i_in;
+}
+int internal_TC_K2_t(int i_in){
+    return i_in;
+}
+int internal_TC_K3_t(int i_in){
+    return i_in;
+}
+
+
+
+#endif //KELDYSH_MFRG_INTERNAL_SYMMETRIES_H
