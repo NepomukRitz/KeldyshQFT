73e4cd8397aaf9aeb5cf511acd2513c60b32426e
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-11-19 18:51:24 +0100

    .

--- Keldysh_mfRG/loop.h
+++ Keldysh_mfRG/loop.h
@@ -12,6 +12,8 @@
 
 //TODO include spinvertex contributions!!
 
+//TODO think how to actually impose equal time constraints!
+
 template <typename Q, typename T >
 class IntegrandR{
     Vertex<T>& vertex;
@@ -28,17 +30,17 @@ public:
         Q resp1 = vertex.densvertex.avertex.value(3, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) ;   //Result should always be real
         Q resp2 = vertex.densvertex.pvertex.value(3, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) ;                              //Should always be 0
         Q resp3 = vertex.densvertex.tvertex.value(3, 0., wp, w, i_in, vertex.densvertex.avertex) ;                      //Equals resp7
-        Q resp4 = vertex.densvertex.irred.vval(3);                                                                      //Always 0.
+        Q resp4 = 0.;  //vertex.densvertex.irred.vval(3);                                                                      //Always 0.
 
         Q resp5 = vertex.densvertex.avertex.value(6, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);    //Should always be 0.
         Q resp6 = vertex.densvertex.pvertex.value(6, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                               //Result should always be real
         Q resp7 = vertex.densvertex.tvertex.value(6, 0., wp, w, i_in, vertex.densvertex.avertex);                       //Equals resp3
-        Q resp8 = vertex.densvertex.irred.vval(6);                                                                      //Always 0.
+        Q resp8 = 0.;  //vertex.densvertex.irred.vval(6);                                                                      //Always 0.
 
         Q resp9  = vertex.densvertex.avertex.value(7, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);   //Something
         Q resp10 = vertex.densvertex.pvertex.value(7, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                              //Something
         Q resp11 = vertex.densvertex.tvertex.value(7, 0., wp, w, i_in, vertex.densvertex.avertex);                      //Something
-        Q resp12 = vertex.densvertex.irred.vval(7);                                                                     //Always 0.5*U
+        Q resp12 = vertex.densvertex.irred.vval(7);  // =0 ?                                                            //Always 0.5*U
 
         Q aid1 = propagator.pvalsmooth(0, wp);
         Q aid2 = conj(propagator.pvalsmooth(0, wp));
@@ -84,17 +86,17 @@ public:
         Q resp1 = vertex.densvertex.avertex.value(1, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) ;
         Q resp2 = vertex.densvertex.pvertex.value(1, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) ;
         Q resp3 = vertex.densvertex.tvertex.value(1, 0., wp, w, i_in, vertex.densvertex.avertex) ;
-        Q resp4 = vertex.densvertex.irred.vval(1);                                                                      //Always 0.5*U
+        Q resp4 = vertex.densvertex.irred.vval(1);      //  =0.?                                                        //Always 0.5*U
 
         Q resp5 = vertex.densvertex.avertex.value(4, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);    //"Similar" to resp1 (i.e. T3(resp1))
         Q resp6 = vertex.densvertex.pvertex.value(4, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                               //"Similar" to resp2 (i.e. TC(resp2))
         Q resp7 = vertex.densvertex.tvertex.value(4, 0., wp, w, i_in, vertex.densvertex.avertex);                       //Equal to resp3
-        Q resp8 = vertex.densvertex.irred.vval(4);                                                                      //Always 0.5*U
+        Q resp8 = vertex.densvertex.irred.vval(4);      // =0.?                                                         //Always 0.5*U
 
         Q resp9  = vertex.densvertex.avertex.value(5, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);   //Note this value is real
         Q resp10 = vertex.densvertex.pvertex.value(5, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                              //Note this value is real
         Q resp11 = vertex.densvertex.tvertex.value(5, 0., wp, w, i_in, vertex.densvertex.avertex);                      //Should always be 0.
-        Q resp12 = vertex.densvertex.irred.vval(5);                                                                      //Always 0.
+        Q resp12 = 0.;  //vertex.densvertex.irred.vval(5);                                                                      //Always 0.
 
         Q aid1 = propagator.pvalsmooth(0, wp);
         Q aid2 = conj(propagator.pvalsmooth(0, wp));
