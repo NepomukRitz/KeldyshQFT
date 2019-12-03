0795057d4a437721c9c7cb1e6ab73a6ce442d6d9
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-12-03 16:29:21 +0100

    Little updates to the convertToIndepIndex function

--- Keldysh_mfRG/Keldysh_symmetries.h
+++ Keldysh_mfRG/Keldysh_symmetries.h
@@ -249,27 +249,20 @@ bool isInList (int iK, vector<int>& list)
 
 /* This function converts indices in the range 0...5 to the actual Keldysh index they correspond to
  * Rule: {0,1,2,3,4,5} -> {0,1,3,5,6,7}
- * The components 0,1,3,5,6 and 7 are the chosen reference components, numerated in the 0...15 "convention"*/
-int convertToRealIndex(int index)
-{
-    if(index ==0 || index == 1)
-        return index;
-    else if(index == 2)
-        return 3;
-    else
-        return index + 2;
-}
-
+ * The components 0,1,3,5,6 and 7 are the chosen reference components, numerated in the 0...15 convention*/
 int convertToIndepIndex(int iK)
 {
-    if(iK ==0 || iK ==1)
-        return iK;
-    else if(iK==3)
-        return 2;
-    else if(iK==5)
-        return 3;
-    else if(iK ==7)
-        return 4;
+    if(isInList(iK, non_zero_Keldysh_K3)) {
+        if (iK == 0 || iK == 1)                                             //  iK(indep)   Real index
+            return iK;                                                      //  0           0
+        else if (iK == 3)                                                   //  1           1
+            return 2;                                                       //  2           3
+        else                                                                //  3           5
+            return iK - 2;                                                  //  4           6
+    } else{                                                                 //  5           7
+        cout << "convertToIndepIndex is throwing this error";
+        return -1;
+    }
 }
 
 /*This function returns the values of the 4 alphas for a given index in the 0...15 set */
