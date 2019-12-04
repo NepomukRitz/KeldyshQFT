dbca41dd3e23b70027f7669a87789f399f95ee78
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-12-04 11:44:56 +0100

    .

--- Keldysh_mfRG/loop.h
+++ Keldysh_mfRG/loop.h
@@ -30,12 +30,12 @@ public:
         Q resp1 = vertex.densvertex.avertex.value(3, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex) ;   //Result should always be real
         Q resp2 = vertex.densvertex.pvertex.value(3, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in) ;                              //Should always be 0
         Q resp3 = vertex.densvertex.tvertex.value(3, 0., wp, w, i_in, vertex.densvertex.avertex) ;                      //Equals resp7
-        Q resp4 = 0.;  //vertex.densvertex.irred.vval(3);                                                                      //Always 0.
+        Q resp4 = vertex.densvertex.irred.vval(3);                                                                      //Always 0.
 
         Q resp5 = vertex.densvertex.avertex.value(6, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);    //Should always be 0.
         Q resp6 = vertex.densvertex.pvertex.value(6, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                               //Result should always be real
         Q resp7 = vertex.densvertex.tvertex.value(6, 0., wp, w, i_in, vertex.densvertex.avertex);                       //Equals resp3
-        Q resp8 = 0.;  //vertex.densvertex.irred.vval(6);                                                                      //Always 0.
+        Q resp8 = vertex.densvertex.irred.vval(6);                                                                      //Always 0.
 
         Q resp9  = vertex.densvertex.avertex.value(7, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);   //Something
         Q resp10 = vertex.densvertex.pvertex.value(7, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                              //Something
@@ -96,7 +96,7 @@ public:
         Q resp9  = vertex.densvertex.avertex.value(5, wp-w, 0.5*(w+wp), 0.5*(w+wp), i_in, vertex.densvertex.tvertex);   //Note this value is real
         Q resp10 = vertex.densvertex.pvertex.value(5, wp+w, 0.5*(w-wp), 0.5*(w-wp), i_in);                              //Note this value is real
         Q resp11 = vertex.densvertex.tvertex.value(5, 0., wp, w, i_in, vertex.densvertex.avertex);                      //Should always be 0.
-        Q resp12 = 0.;  //vertex.densvertex.irred.vval(5);                                                                      //Always 0.
+        Q resp12 = vertex.densvertex.irred.vval(5);                                                                      //Always 0.
 
         Q aid1 = propagator.pvalsmooth(0, wp);
         Q aid2 = conj(propagator.pvalsmooth(0, wp));
@@ -140,8 +140,8 @@ auto loop(Vertex<fullvert<comp> >& fullvertex, Propagator& prop) -> SelfEnergy<c
         IntegrandR<comp, fullvert<comp> > integrandR(fullvertex, prop, w, i_in);
         IntegrandK<comp, fullvert<comp> > integrandK(fullvertex, prop, w, i_in);
 
-        comp integratedR = (-(comp)1.i/(2.*pi))*integrator(integrandR, 1.5*w_lower_f, 1.5*w_upper_f);
-        comp integratedK = (-(comp)1.i/(2.*pi))*integrator(integrandK, 1.5*w_lower_f, 1.5*w_upper_f);
+        comp integratedR = (-(comp)1.i/(2.*pi))*integrator(integrandR, w_lower_f, w_upper_f);
+        comp integratedK = (-(comp)1.i/(2.*pi))*integrator(integrandK, w_lower_f, w_upper_f);
 
         resp.setself(0, i, integratedR);
         resp.setself(1, i, integratedK);
