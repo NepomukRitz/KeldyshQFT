625811136e07e58f433d3f826cec9379f9157982
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2019-12-10 14:16:06 +0100

    .

---
+++ Keldysh_mfRG/cmake-build-debug/CMakeFiles/openmp_test_clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openmp_test_clion.dir/main.cpp.o"
+  "openmp_test_clion"
+  "openmp_test_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openmp_test_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
