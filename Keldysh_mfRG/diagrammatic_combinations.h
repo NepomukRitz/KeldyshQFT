05a0dfadde72917edb1bff5fe8599bf9077350a5
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-01-15 18:04:33 +0100

    Implemented all combinations of functions left/right same/different bare

--- Keldysh_mfRG/diagrammatic_combinations.h
+++ Keldysh_mfRG/diagrammatic_combinations.h
@@ -8,7 +8,19 @@
 #include "vertex.h"
 
 template <typename Q, char channel>
-auto left_same_bare (Vertex<fullvert<Q> >& vertex, int i1, double w, double vpp, int i_in) -> Q {}
+auto left_same_bare (Vertex<fullvert<Q> >& vertex, int i1, double w, double vpp, int i_in) -> Q;
+
+template <typename Q, char channel>
+auto right_same_bare (Vertex<fullvert<Q> >& vertex, int i3, double w, double vpp, int i_in) -> Q;
+
+template <typename Q, char channel>
+auto left_diff_bare (Vertex<fullvert<Q> >& vertex, int i1, double w, double v, double vpp, int i_in) -> Q;
+
+template <typename Q, char channel>
+auto right_diff_bare (Vertex<fullvert<Q> >& vertex, int i3, double w, double vp, double vpp, int i_in) -> Q;
+
+
+
 
 template <>
 auto left_same_bare<comp, 'a'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double vpp, int i_in) -> comp {
@@ -16,22 +28,85 @@ auto left_same_bare<comp, 'a'> (Vertex<fullvert<comp> >& vertex, int i1, double 
            + vertex.densvertex.avertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.tvertex)
            + vertex.densvertex.avertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.tvertex);
 }
-
 template <>
 auto left_same_bare<comp, 'p'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double vpp, int i_in) -> comp {
     return vertex.densvertex.irred.vval(i1, i_in)
            + vertex.densvertex.pvertex.K1_vvalsmooth(i1, w, i_in)
            + vertex.densvertex.pvertex.K2b_vvalsmooth(i1, w, vpp, i_in);
 }
+template <>
+auto left_same_bare<comp, 't'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double vpp, int i_in) -> comp {
+    return vertex.densvertex.irred.vval(i1, i_in)
+           + vertex.densvertex.tvertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.tvertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.avertex);
+}
+
+
+template <>
+auto right_same_bare<comp, 'a'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vpp, int i_in) -> comp {
+    return vertex.densvertex.irred.vval(i3, i_in)
+           + vertex.densvertex.avertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.tvertex)
+           + vertex.densvertex.avertex.K2_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.tvertex);
+}
+template <>
+auto right_same_bare<comp, 'p'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vpp, int i_in) -> comp {
+    return vertex.densvertex.irred.vval(i3, i_in)
+           + vertex.densvertex.pvertex.K1_vvalsmooth(i3, w, i_in)
+           + vertex.densvertex.pvertex.K2_vvalsmooth(i3, w, vpp, i_in);
+}
+template <>
+auto right_same_bare<comp, 't'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vpp, int i_in) -> comp {
+    return vertex.densvertex.irred.vval(i3, i_in)
+           + vertex.densvertex.tvertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.tvertex.K2_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.avertex);
+}
 
 
 
-resp1 = vertex1.densvertex.irred.vval(i1, i_in);
-resp2 = vertex1.densvertex.avertex.K1_vvalsmooth(i1, wa, i_in, vertex1.densvertex.tvertex);
-resp3 = vertex1.densvertex.avertex.K2b_vvalsmooth(i1, wa, vppa, i_in, vertex1.densvertex.tvertex);
-resp4 = vertex2.densvertex.irred.vval(i3, i_in);
-resp5 = vertex2.densvertex.avertex.K1_vvalsmooth(i3, wa, i_in, vertex2.densvertex.tvertex);
-resp6 = vertex2.densvertex.avertex.K2_vvalsmooth(i3, wa, vppa, i_in, vertex2.densvertex.tvertex);
+template <>
+auto left_diff_bare<comp, 'a'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double v, double vpp, int i_in) -> comp {
+    return vertex.densvertex.avertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.tvertex)
+           + vertex.densvertex.avertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.tvertex)
+           + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'a');
+
+}
+template <>
+auto left_diff_bare<comp, 'p'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double v, double vpp, int i_in) -> comp {
+    return vertex.densvertex.pvertex.K2_vvalsmooth(i1, w, v, i_in)
+           + vertex.densvertex.pvertex.K3_vvalsmooth(i1, w, v, vpp, i_in)
+           + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'p');
+
+}
+template <>
+auto left_diff_bare<comp, 't'> (Vertex<fullvert<comp> >& vertex, int i1, double w, double v, double vpp, int i_in) -> comp {
+    return vertex.densvertex.tvertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.tvertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 't');
+
+}
+
+
+template <>
+auto right_diff_bare<comp, 'a'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vp, double vpp, int i_in) -> comp {
+    return vertex.densvertex.avertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.tvertex)
+           + vertex.densvertex.avertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.tvertex)
+           + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'a');
+
+}
+template <>
+auto right_diff_bare<comp, 'p'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vp, double vpp, int i_in) -> comp {
+    return vertex.densvertex.pvertex.K2b_vvalsmooth(i3, w, vp, i_in)
+           + vertex.densvertex.pvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in)
+           + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'p');
+
+}
+template <>
+auto right_diff_bare<comp, 't'> (Vertex<fullvert<comp> >& vertex, int i3, double w, double vp, double vpp, int i_in) -> comp {
+    return vertex.densvertex.tvertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.tvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.avertex)
+           + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 't');
+
+}
 
 
 #endif //KELDYSH_MFRG_DIAGRAMMATIC_COMBINATIONS_H
