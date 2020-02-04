72a507881946f8603b25df0393223dfded30c2e7
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-04 18:19:52 +0100

    Added file with dedicated functions for interpolations

---
+++ Keldysh_mfRG/interpolations.h
@@ -0,0 +1,83 @@
+//
+// Created by Sa.Aguirre on 2/4/20.
+//
+
+#ifndef KELDYSH_MFRG_INTERPOLATIONS_H
+#define KELDYSH_MFRG_INTERPOLATIONS_H
+
+#include "frequency_grid.h"
+#include "parameters.h"
+#include "data_structures.h"
+//#include "a_vertex.h"
+//#include "p_vertex.h"
+//#include "t_vertex.h"
+
+template <typename Q, typename T>
+void interpolateK1(Q& ans, double pf, int iK, double w, int i_in, T& vertex){
+    int index = fconv_bos(w);
+    double x1 = bfreqs[index];
+    double x2 = bfreqs[index] + dw;
+    double xd = (w - x1) / (x2 - x1);
+
+    Q f1 = vertex.K1_vval(iK, index, i_in);
+    Q f2 = vertex.K1_vval(iK, index + 1, i_in);
+
+    ans = pf * ((1. - xd) * f1 + xd * f2);
+}
+
+template <typename Q, typename T>
+void interpolateK2 (Q& ans, double pf, int iK, double w, double v, int i_in, T& vertex){
+    int index_b = fconv_bos(w);
+    int index_f = fconv_fer(v);
+
+    double x1 = bfreqs[index_b];
+    double x2 = bfreqs[index_b] + dw;
+    double y1 = ffreqs[index_f];
+    double y2 = ffreqs[index_f] + dv;
+    double xd = (w - x1) / (x2 - x1);
+    double yd = (v - y1) / (y2 - y1);
+
+    Q f11 = vertex.K2_vval(iK, index_b, index_f, i_in);
+    Q f12 = vertex.K2_vval(iK, index_b, index_f + 1, i_in);
+    Q f21 = vertex.K2_vval(iK, index_b + 1, index_f, i_in);
+    Q f22 = vertex.K2_vval(iK, index_b + 1, index_f + 1, i_in);
+
+    ans = pf * ((1. - yd) * ((1. - xd) * f11 + xd * f21) + yd * ((1. - xd) * f12 + xd * f22));
+}
+
+template <typename Q, typename T>
+void interpolateK3 (Q& ans, double pf, int iK, double w, double v1, double v2, int i_in, T& vertex) {
+    int index_b = fconv_bos(w);
+    int index_f1=fconv_fer(v1);
+    int index_f2=fconv_fer(v2);
+
+    double x1 = bfreqs[index_b];
+    double x2 = bfreqs[index_b] + dw;
+    double y1 = ffreqs[index_f1];
+    double y2 = ffreqs[index_f1] + dv;
+    double z1 = ffreqs[index_f2];
+    double z2 = ffreqs[index_f2] + dv;
+    double xd = (w - x1) / (x2 - x1);
+    double yd = (v1 - y1) / (y2 - y1);
+    double zd = (v2- z1) / (z2 - z1);
+
+    Q f111 = vertex.K3_vval(iK, index_b, index_f1, index_f2, i_in);
+    Q f112 = vertex.K3_vval(iK, index_b, index_f1, index_f2 + 1, i_in);
+    Q f121 = vertex.K3_vval(iK, index_b, index_f1 + 1, index_f2, i_in);
+    Q f122 = vertex.K3_vval(iK, index_b, index_f1 + 1, index_f2 + 1, i_in);
+    Q f211 = vertex.K3_vval(iK, index_b + 1, index_f1, index_f2, i_in);
+    Q f212 = vertex.K3_vval(iK, index_b + 1, index_f1, index_f2 + 1, i_in);
+    Q f221 = vertex.K3_vval(iK, index_b + 1, index_f1 + 1, index_f2, i_in);
+    Q f222 = vertex.K3_vval(iK, index_b + 1, index_f1 + 1, index_f2 + 1, i_in);
+
+    Q c00 = f111 * (1. - xd) + f211 * xd;
+    Q c01 = f112 * (1. - xd) + f212 * xd;
+    Q c10 = f121 * (1. - xd) + f221 * xd;
+    Q c11 = f122 * (1. - xd) + f222 * xd;
+    Q c0 = c00 * (1. - yd) + c10 * yd;
+    Q c1 = c01 * (1. - yd) + c11 * yd;
+
+    ans = pf * (c0 * (1. - zd) + c1 * zd);
+}
+
+#endif //KELDYSH_MFRG_INTERPOLATIONS_H
