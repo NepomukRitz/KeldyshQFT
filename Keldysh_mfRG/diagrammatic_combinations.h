47c9a23ce2e2882b3f68d8065ca408f44e19cd8b
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-04 21:07:12 +0100

    Extended functions to support preprocessor macro fro number of diagrammatic classes

--- Keldysh_mfRG/diagrammatic_combinations.h
+++ Keldysh_mfRG/diagrammatic_combinations.h
@@ -21,45 +21,81 @@
 template <typename Q>
 auto left_same_bare (Vertex<fullvert<Q> >& vertex, int i1, double w, double vpp, int i_in, char channel) -> Q
 {
+    Q gamma0, K1, K2b;
     switch (channel){
         case 'a':
-            return vertex.densvertex.irred.vval(i1, i_in)
-                   + vertex.densvertex.avertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.avertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.tvertex);
+            gamma0 = vertex.densvertex.irred.vval(i1, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.avertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.tvertex);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.avertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.tvertex);
+#endif
+            break;
+
         case 'p':
-            return vertex.densvertex.irred.vval(i1, i_in)
-                   + vertex.densvertex.pvertex.K1_vvalsmooth(i1, w, i_in)
-                   + vertex.densvertex.pvertex.K2b_vvalsmooth(i1, w, vpp, i_in);
+            gamma0 = vertex.densvertex.irred.vval(i1, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.pvertex.K1_vvalsmooth(i1, w, i_in);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.pvertex.K2b_vvalsmooth(i1, w, vpp, i_in);
+#endif
+            break;
         case 't' :
-            return vertex.densvertex.irred.vval(i1, i_in)
-                   + vertex.densvertex.tvertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.tvertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.avertex);
+            gamma0 = vertex.densvertex.irred.vval(i1, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.tvertex.K1_vvalsmooth(i1, w, i_in, vertex.densvertex.avertex);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.tvertex.K2b_vvalsmooth(i1, w, vpp, i_in, vertex.densvertex.avertex);
+#endif
+            break;
         default:
             return 0.;
 
     }
+    return gamma0 + K1 + K2b;
 }
 
 template <typename Q>
 auto right_same_bare (Vertex<fullvert<Q> >& vertex, int i3, double w, double vpp, int i_in, char channel) -> Q
 {
+    Q gamma0, K1, K2b;
     switch (channel){
         case 'a':
-            return vertex.densvertex.irred.vval(i3, i_in)
-                   + vertex.densvertex.avertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.avertex.K2_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.tvertex);
+            gamma0 = vertex.densvertex.irred.vval(i3, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.avertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.tvertex);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.avertex.K2b_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.tvertex);
+#endif
+            break;
+
         case 'p':
-            return vertex.densvertex.irred.vval(i3, i_in)
-                   + vertex.densvertex.pvertex.K1_vvalsmooth(i3, w, i_in)
-                   + vertex.densvertex.pvertex.K2_vvalsmooth(i3, w, vpp, i_in);
+            gamma0 = vertex.densvertex.irred.vval(i3, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.pvertex.K1_vvalsmooth(i3, w, i_in);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.pvertex.K2b_vvalsmooth(i3, w, vpp, i_in);
+#endif
+            break;
         case 't' :
-            return vertex.densvertex.irred.vval(i3, i_in)
-                   + vertex.densvertex.tvertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.tvertex.K2_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.avertex);
+            gamma0 = vertex.densvertex.irred.vval(i3, i_in);
+#if DIAG_CLASS >=1
+            K1 = vertex.densvertex.tvertex.K1_vvalsmooth(i3, w, i_in, vertex.densvertex.avertex);
+#endif
+#if DIAG_CLASS >=2
+            K2b = vertex.densvertex.tvertex.K2b_vvalsmooth(i3, w, vpp, i_in, vertex.densvertex.avertex);
+#endif
+            break;
         default:
             return 0.;
 
     }
+    return gamma0 + K1 + K2b;
 }
 
 
@@ -106,43 +142,79 @@ auto right_same_bare (Vertex<fullvert<Q> >& vertex, int i3, double w, double vpp
 
 template <typename Q>
 auto left_diff_bare (Vertex<fullvert<Q> >& vertex, int i1, double w, double v, double vpp, int i_in, char channel) -> Q {
+//TODO is gammaRb in K2 and in K3 or only in K3? Or ae there contributions in both cases, but aren't the same?
+    Q K2, K3, gammaRb;
 
     switch (channel){
         case 'a' :
-            return vertex.densvertex.avertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.avertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'a');
+#if DIAG_CLASS >=2
+            K2 = vertex.densvertex.avertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.tvertex);
+            gammaRb = vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'a');
+#endif
+#if DIAG_CLASS >=3
+            K3 = vertex.densvertex.avertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.tvertex);
+#endif
+            break;
         case 'p':
-            return vertex.densvertex.pvertex.K2_vvalsmooth(i1, w, v, i_in)
-                   + vertex.densvertex.pvertex.K3_vvalsmooth(i1, w, v, vpp, i_in)
-                   + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'p');
+#if DIAG_CLASS >=2
+            K2 = vertex.densvertex.pvertex.K2_vvalsmooth(i1, w, v, i_in);
+            gammaRb = vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 'p');
+#endif
+#if DIAG_CLASS >=3
+            K3 = vertex.densvertex.pvertex.K3_vvalsmooth(i1, w, v, vpp, i_in);
+#endif
+            break;
         case 't':
-            return vertex.densvertex.tvertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.tvertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 't');
+#if DIAG_CLASS >=2
+            K2 = vertex.densvertex.tvertex.K2_vvalsmooth(i1, w, v, i_in, vertex.densvertex.avertex);
+            gammaRb = vertex.densvertex.gammaRb(i1, w, v, vpp, i_in, 't');;
+#endif
+#if DIAG_CLASS >=3
+            K3 = vertex.densvertex.tvertex.K3_vvalsmooth(i1, w, v, vpp, i_in, vertex.densvertex.avertex);
+            break;
         default:
             return 0.;
     }
+    return K2 + K3 + gammaRb;
 }
 
 template <typename Q>
 auto right_diff_bare (Vertex<fullvert<Q> >& vertex, int i3, double w, double vp, double vpp, int i_in, char channel) -> Q {
+    //TODO is gammaRb in K2 and in K3 or only in K3? Or ae there contributions in both cases, but aren't the same?
+    Q K2b, K3, gammaRb;
+
     switch (channel){
         case 'a' :
-            return vertex.densvertex.avertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.avertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.tvertex)
-                   + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'a');
+#if DIAG_CLASS >= 2
+            K2b = vertex.densvertex.avertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.tvertex);
+            gammaRb = vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'a');
+#endif
+#if DIAG_CLASS >= 3
+            K3 = vertex.densvertex.avertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.tvertex);
+#endif
+            break;
         case 'p':
-            return vertex.densvertex.pvertex.K2b_vvalsmooth(i3, w, vp, i_in)
-                   + vertex.densvertex.pvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in)
-                   + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'p');
+#if DIAG_CLASS >= 2
+            K2b = vertex.densvertex.pvertex.K2b_vvalsmooth(i3, w, vp, i_in);
+            gammaRb = vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 'p');
+#endif
+#if DIAG_CLASS >= 3
+            K3 = vertex.densvertex.pvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in);
+#endif
+            break;
         case 't':
-            return vertex.densvertex.tvertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.tvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.avertex)
-                   + vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 't');
+#if DIAG_CLASS >= 2
+            K2b = vertex.densvertex.tvertex.K2b_vvalsmooth(i3, w, vp, i_in, vertex.densvertex.avertex);
+            gammaRb = vertex.densvertex.gammaRb(i3, w, vpp, vp, i_in, 't');
+#endif
+#if DIAG_CLASS >= 3
+            K3 = vertex.densvertex.tvertex.K3_vvalsmooth(i3, w, vpp, vp, i_in, vertex.densvertex.avertex);
+#endif
+            break;
         default:
             return 0.;
     }
+    return K2b + K3 + gammaRb;
 }
 
 
