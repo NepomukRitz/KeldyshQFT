0ae8a5f811ce67f299c2faa12257c35cbf7db1b5
E.Walter <E.Walter@th-ws-740m18.theorie.physik.uni-muenchen.de>
2020-02-10 19:02:00 +0100

    documentation/commenting bubbles

--- Keldysh_mfRG/bubbles.h
+++ Keldysh_mfRG/bubbles.h
@@ -1,6 +1,14 @@
-//
-// Created by Elias Walter on 2020-01-09.
-//
+/**
+ * Classes / functions for computing bubbles.
+ *
+ * class Bubble            : Object containing two propagators, with call operator
+ * class Integrand_Ki      : Object with call operator providing the integrand for bubble frequency integration
+ *                           in diagrammatic class Ki:
+ *                           Computing Vertex * Bubble * Vertex, and performing the internal Keldysh summation
+ * class Integrand_Ki_diff : Same as Integrand_Ki, for differentiated bubble
+ * bubble_function()       : Computing the bubble frequency integral, using the integrand classes and the integrator
+ *                           from "integrator.h". Using MPI+OMP parallelization in the external arguments.
+ */
 
 #ifndef KELDYSH_MFRG_BUBBLES_H
 #define KELDYSH_MFRG_BUBBLES_H
@@ -14,16 +22,31 @@
 #include "diagrammatic_combinations.h"
 
 
-
+/**
+ * Class combining two propagators, either GG or GS+SG
+ */
 class Bubble{
     Propagator& g;
     Propagator& s;
     bool dot;
 public:
 
+    /**
+     * Constructor:
+     * @param propagatorG : first propagator (always a standard one)
+     * @param propagatorS : second propagator (standard or single-scale/differentiated, depending on "dot_in")
+     * @param dot_in      : whether to compute standard (false) or differentiated (true) bubble
+     */
     Bubble(Propagator& propagatorG, Propagator& propagatorS, bool dot_in)
         :g(propagatorG), s(propagatorS), dot(dot_in) {};
 
+    /**
+     * Call operator:
+     * @param iK    : Keldysh index of combined bubble object (0 <= iK <= 15)
+     * @param v1    : frequency of first propagator
+     * @param v2    : frequency of second propagator
+     * @return comp : value of the bubble evaluated at (iK, v1, v2)
+     */
     auto value(int iK, double v1, double v2) -> comp{
         comp ans;
         if(dot){
@@ -96,7 +119,9 @@ public:
 };
 
 
-
+/**
+ * Integrand classes for non-differentiated bubble contributing to diagrammatic class K1, K2, K3
+ */
 template <typename Q> class Integrand_K1 {
     Vertex<fullvert<Q> >& vertex1;
     Vertex<fullvert<Q> >& vertex2;
@@ -105,9 +130,21 @@ template <typename Q> class Integrand_K1 {
     char channel;
     double w;
 public:
+    /**
+     * Constructor:
+     * @param vertex1_in : left vertex
+     * @param vertex2_in : right vertex
+     * @param Pi_in      : Bubble object connecting the left and right vertex
+     * @param i0_in      : index (0,...,5) specifying the (external) Keldysh component of integrand object
+     *                     (converted into actual Keldysh index i0 within the constructor)
+     * @param w_in       : external bosonic frequency \omega
+     * @param i_in_in    : external index for internal structure
+     * @param ch_in      : diagrammatic channel ('a', 'p', 't')
+     */
     Integrand_K1(Vertex<fullvert<Q> >& vertex1_in, Vertex<fullvert<Q> >& vertex2_in, Bubble& Pi_in, int i0_in, double w_in,    int i_in_in, char ch_in)
         :                     vertex1(vertex1_in),              vertex2(vertex2_in),     Pi(Pi_in),                w(w_in),  i_in(i_in_in), channel(ch_in)
     {
+        // converting index i0_in (0,...,5) into actual Keldysh index i0 (0,...,15)
         switch (channel) {
             case 'a': i0 = non_zero_Keldysh_K1a[i0_in]; break;
             case 'p': i0 = non_zero_Keldysh_K1p[i0_in]; break;
@@ -116,6 +153,11 @@ public:
         }
     };
 
+    /**
+     * Call operator:
+     * @param vpp : frequency at which to evaluate integrand (to be integrated over)
+     * @return Q  : value of the integrand object evaluated at frequency vpp (comp or double)
+     */
     auto operator() (double vpp) -> Q {
 
         Q res;
@@ -155,9 +197,24 @@ template <typename Q> class Integrand_K2
     char channel, part;
     double w, v;
 public:
+    /**
+     * Constructor:
+     * @param vertex1_in : left vertex
+     * @param vertex2_in : right vertex
+     * @param Pi_in      : Bubble object connecting the left and right vertex
+     * @param i0_in      : index (0,...,5) specifying the (external) Keldysh component of integrand object
+     *                     (converted into actual Keldysh index i0 within the constructor)
+     * @param w_in       : external bosonic frequency \omega
+     * @param v_in       : external fermionic frequency \nu
+     * @param i_in_in    : external index for internal structure
+     * @param ch_in      : diagrammatic channel ('a', 'p', 't')
+     * @param pt_in      : For multi-loop calculation: specify if one computes left ('L') or right ('R')
+     *                     multi-loop contribution.
+     */
     Integrand_K2(Vertex<fullvert<Q> >& vertex1_in, Vertex<fullvert<Q> >& vertex2_in,   Bubble& Pi_in, int i0_in, double w_in, double v_in,   int i_in_in,     char ch_in, char pt_in)
         :                     vertex1(vertex1_in),              vertex2(vertex2_in),       Pi(Pi_in),                w(w_in),     v(v_in), i_in(i_in_in), channel(ch_in), part(pt_in)
     {
+        // converting index i0_in (0,...,5) into actual Keldysh index i0 (0,...,15)
         switch (channel) {
             case 'a': i0 = non_zero_Keldysh_K2a[i0_in]; break;
             case 'p': i0 = non_zero_Keldysh_K2p[i0_in]; break;
@@ -166,8 +223,13 @@ public:
         }
     };
 
+    /**
+     * Call operator:
+     * @param vpp : frequency at which to evaluate integrand (to be integrated over)
+     * @return Q  : value of the integrand object evaluated at frequency vpp (comp or double)
+     */
     auto operator() (double vpp) -> Q {
-        if (part != 'L') return 0.;
+        if (part != 'L') return 0.;  // left part of multi-loop contribution does not contribute to K2 class
 
         Q res, res_l_V, res_r_V, res_l_Vhat, res_r_Vhat;
         Q Pival;
@@ -220,12 +282,32 @@ template <typename Q> class Integrand_K3
     char channel, part;
     double w, v, vp;
 public:
+    /**
+     * Constructor:
+     * @param vertex1_in : left vertex
+     * @param vertex2_in : right vertex
+     * @param Pi_in      : Bubble object connecting the left and right vertex
+     * @param i0_in      : index (0,...,5) specifying the (external) Keldysh component of integrand object
+     *                     (converted into actual Keldysh index i0 within the constructor)
+     * @param w_in       : external bosonic frequency \omega
+     * @param v_in       : external fermionic frequency \nu
+     * @param vp_in      : external fermionic frequency \nu'
+     * @param i_in_in    : external index for internal structure
+     * @param ch_in      : diagrammatic channel ('a', 'p', 't')
+     * @param pt_in      : For multi-loop calculation: specify if one computes left ('L') or right ('R')
+     *                     multi-loop contribution.
+     */
     Integrand_K3(Vertex<fullvert<Q> >& vertex1_in, Vertex<fullvert<Q> >& vertex2_in, Bubble& Pi_in, int i0_in, double w_in, double v_in, double vp_in, int i_in_in, char ch_in, char pt_in)
         :                     vertex1(vertex1_in),              vertex2(vertex2_in),     Pi(Pi_in),                 w(w_in),     v(v_in),    vp(vp_in), i_in(i_in_in), channel(ch_in), part(pt_in)
     {
-        i0 = non_zero_Keldysh_K3[i0_in];
+        i0 = non_zero_Keldysh_K3[i0_in]; // converting index i0_in (0,...,5) into actual Keldysh index i0 (0,...,15)
     };
 
+    /**
+     * Call operator:
+     * @param vpp : frequency at which to evaluate integrand (to be integrated over)
+     * @return Q  : value of the integrand object evaluated at frequency vpp (comp or double)
+     */
     auto operator() (double vpp) -> Q {
         Q res, res_l_V, res_r_V,  res_l_Vhat, res_r_Vhat;
         Q Pival;
@@ -294,6 +376,9 @@ public:
     }
 };
 
+/**
+ * Integrand classes for differentiated bubble contributing to diagrammatic class K1, K2, K3
+ */
 template <typename Q> class Integrand_K1_diff {
     Vertex<fullvert<Q> >& vertex1;
     Vertex<fullvert<Q> >& vertex2;
@@ -487,14 +572,33 @@ public:
 };
 
 
+/**
+ * Function that computes the bubble frequency integral for all external parameters,
+ * including MPI and OMP parallelization. The first (reference) argument contains the result.
+ * Distributing the tasks between MPI/OMP is done explicitly before each for-loop for each diagrammatic class
+ * K1, K2, K3. (TODO: handle this globally / in parameters.h?)
+ *
+ * @tparam Q      : data type (comp or double)
+ * @param dgamma  : result (full Vertex object)
+ * @param vertex1 : left vertex in bubble
+ * @param vertex2 : right vertex in bubble
+ * @param G       : first propagator of bubble
+ * @param S       : second propagator of bubble
+ * @param channel : diagrammatic channel ('a', 'p', or 't')
+ * @param diff    : whether or not the bubble is a differentiated one
+ * @param part    : For multi-loop calculation: specify if one computes left ('L') or right ('R')
+ *                  multi-loop contribution. Use '.' for first loop order.
+ */
 template <typename Q>
-void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1, Vertex<fullvert<Q> >& vertex2, Propagator& G, Propagator& S, char channel, bool diff, char part)
+void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1, Vertex<fullvert<Q> >& vertex2,
+                     Propagator& G, Propagator& S, char channel, bool diff, char part)
 {
-    Bubble Pi(G,S, diff);
+    Bubble Pi(G, S, diff); // initialize bubble object
 
     int nw1_w = 0, nw2_w = 0, nw2_v = 0, nw3_w = 0, nw3_v = 0, nw3_vp = 0;
     Q prefactor;
 
+    // set channel-specific frequency ranges and prefactor (1, 1/2, -1 for a, p, t)
     switch (channel) {
         case 'a':
             nw1_w = nw1_wa;
@@ -526,23 +630,26 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
         default: ;
     }
 
-    int mpi_size = mpi_world_size();
-    int mpi_rank = mpi_world_rank();
+    int mpi_size = mpi_world_size(); // number of mpi processes
+    int mpi_rank = mpi_world_rank(); // number of the current mpi process
 
 #ifdef DIAG_CLASS
 #if DIAG_CLASS>=1
     double tK1 = get_time();
     /*K1 contributions*/
-    int n_mpi = nK_K1 * nw1_w;
-    int n_omp = n_in;
+    int n_mpi = nK_K1 * nw1_w; // set external arguments for MPI-parallelization (# of tasks distributed via MPI)
+    int n_omp = n_in;          // set external arguments for OMP-parallelization (# of tasks per MPI-task distributed via OMP)
 
+    // initialize buffer into which each MPI process writes their results
     vec<Q> K1_buffer = mpi_initialize_buffer<Q>(n_mpi, n_omp);
 
+    // start for-loop over external arguments, using MPI and OMP
     int iterator = 0;
     for (int i_mpi=0; i_mpi<n_mpi; ++i_mpi) {
         if (i_mpi % mpi_size == mpi_rank) {
 #pragma omp parallel for
             for (int i_omp=0; i_omp<n_omp; ++i_omp) {
+                // converting external MPI/OMP indices to physical indices (TODO: put into extra function(s)?)
                 int iK1 = i_mpi * n_omp + i_omp;
                 int i0 = iK1/(nw1_w*n_in);
                 int iw = iK1/(n_in) - i0*nw1_w;
@@ -550,21 +657,24 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
                 double w = bfreqs[iw];
                 Q value;
 
+                // initialize the integrand object and perform frequency integration
+                // (distinguishing between differentiated and non-differentiated bubble)
                 if(diff){
                     Integrand_K1_diff<Q> integrand_K1(vertex1, vertex2, Pi, i0, w, i_in, channel);
                     value = prefactor*(-1./(2.*pi*im_unit))*integrator(integrand_K1, w_lower_f, w_upper_f);                      //Integration over a fermionic frequency
-                }
+                } //TODO: prefactor -1./(2.*pi*im_unit) into Integrand classes?
                 else{
                     Integrand_K1<Q> integrand_K1(vertex1, vertex2, Pi, i0, w, i_in, channel);
                     value = prefactor*(-1./(2.*pi*im_unit))*integrator(integrand_K1, w_lower_f, w_upper_f);                      //Integration over a fermionic frequency
                 }
 
-                K1_buffer[iterator*n_omp + i_omp] = value;
+                K1_buffer[iterator*n_omp + i_omp] = value; // write result of integration into MPI buffer
             }
             ++iterator;
         }
     }
 
+    // collect+combine results from different MPI processes, reorder them appropriately
     vec<Q> K1_result = mpi_initialize_result<Q> (n_mpi, n_omp);
     mpi_collect(K1_buffer, K1_result, n_mpi, n_omp);
     vec<Q> K1_ordered_result = mpi_reorder_result(K1_result, n_mpi, n_omp);
@@ -588,13 +698,16 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
     n_mpi = nK_K2 * nw2_w;
     n_omp = nw2_v * n_in;
 
+    // initialize buffer into which each MPI process writes their results
     vec<Q> K2_buffer = mpi_initialize_buffer<Q>(n_mpi, n_omp);
 
+    // start for-loop over external arguments, using MPI and OMP
     iterator = 0;
     for (int i_mpi=0; i_mpi<n_mpi; ++i_mpi) {
         if (i_mpi % mpi_size == mpi_rank) {
 #pragma omp parallel for
             for (int i_omp=0; i_omp<n_omp; ++i_omp) {
+                // converting external MPI/OMP indices to physical indices
                 int iK2 = i_mpi * n_omp + i_omp;
                 int i0 = iK2 /(nw2_w * nw2_v * n_in);
                 int iw = iK2 /(nw2_v * n_in) - i0*nw2_w;
@@ -604,6 +717,8 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
                 double v = ffreqs[iv];
                 Q value;
 
+                // initialize the integrand object and perform frequency integration
+                // (distinguishing between differentiated and non-differentiated bubble)
                 if(diff){
                     Integrand_K2_diff<Q> integrand_K2 (vertex1, vertex2, Pi, i0, w, v, i_in, channel);
                     value = prefactor*(-1./(2.*pi*im_unit))*integrator(integrand_K2, w_lower_f, w_upper_f);                      //Integration over vppp, a fermionic frequency
@@ -613,12 +728,13 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
                     value = prefactor*(-1./(2.*pi*im_unit))*integrator(integrand_K2, w_lower_f, w_upper_f);                      //Integration over vppp, a fermionic frequency
                 }
 
-                K2_buffer[iterator*n_omp + i_omp] = value;
+                K2_buffer[iterator*n_omp + i_omp] = value; // write result of integration into MPI buffer
             }
             ++iterator;
         }
     }
 
+    // collect+combine results from different MPI processes, reorder them appropriately
     vec<Q> K2_result = mpi_initialize_result<Q> (n_mpi, n_omp);
     mpi_collect(K2_buffer, K2_result, n_mpi, n_omp);
     vec<Q> K2_ordered_result = mpi_reorder_result(K2_result, n_mpi, n_omp);
@@ -640,13 +756,16 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
     n_mpi = nK_K3 * nw3_w * nw3_v;
     n_omp = nw3_vp * n_in;
 
+    // initialize buffer into which each MPI process writes their results
     vec<Q> K3_buffer = mpi_initialize_buffer<Q>(n_mpi, n_omp);
 
+    // start for-loop over external arguments, using MPI and OMP
     iterator = 0;
     for (int i_mpi=0; i_mpi<n_mpi; ++i_mpi) {
         if (i_mpi % mpi_size == mpi_rank) {
 #pragma omp parallel for
             for (int i_omp=0; i_omp<n_omp; ++i_omp) {
+                // converting external MPI/OMP indices to physical indices
                 int iK3 = i_mpi * n_omp + i_omp;
                 int i0 = iK3/(nw3_w * nw3_v * nw3_vp * n_in);
                 int iw = iK3/(nw3_v * nw3_vp * n_in) - i0*nw3_w;
@@ -658,7 +777,8 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
                 double vp = ffreqs[ivp];
                 Q value;
 
-
+                // initialize the integrand object and perform frequency integration
+                // (distinguishing between differentiated and non-differentiated bubble)
                 if (diff){
                     Integrand_K3_diff<Q> integrand_K3 (vertex1, vertex2, Pi, i0, w, v, vp, i_in, channel);
 
@@ -670,12 +790,13 @@ void bubble_function(Vertex<fullvert<Q> >& dgamma, Vertex<fullvert<Q> >& vertex1
                     value = prefactor*(-1./(2.*pi*im_unit))*integrator(integrand_K3, w_lower_f, w_upper_f);                      //Integration over vppp, a fermionic frequency
                 }
 
-                K3_buffer[iterator*n_omp + i_omp] = value;
+                K3_buffer[iterator*n_omp + i_omp] = value; // write result of integration into MPI buffer
             }
             ++iterator;
         }
     }
 
+    // collect+combine results from different MPI processes, reorder them appropriately
     vec<Q> K3_result = mpi_initialize_result<Q> (n_mpi, n_omp);
     mpi_collect(K3_buffer, K3_result, n_mpi, n_omp);
     vec<Q> K3_ordered_result = mpi_reorder_result(K3_result, n_mpi, n_omp);
