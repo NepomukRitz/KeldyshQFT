22ac3a9870b1b830fd55c78eaf4601f8fecf479d
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-13 16:32:33 +0100

    Handling internal symmetries through references, much more efficiently

--- Keldysh_mfRG/internal_symmetries.h
+++ Keldysh_mfRG/internal_symmetries.h
@@ -5,121 +5,45 @@
 #ifndef KELDYSH_MFRG_INTERNAL_SYMMETRIES_H
 #define KELDYSH_MFRG_INTERNAL_SYMMETRIES_H
 
-auto internal_T1_K1_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K2_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K3_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K1_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K2_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K3_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K1_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K2_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K3_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K1_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K2_a(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K3_a(int i_in) -> int{
-    return i_in;
-}
+void internal_T1_K1_a(int& i_in){}
+void internal_T2_K1_a(int& i_in){}
+void internal_T3_K1_a(int& i_in){}
+void internal_TC_K1_a(int& i_in){}
+void internal_T1_K1_p(int& i_in){}
+void internal_T2_K1_p(int& i_in){}
+void internal_T3_K1_p(int& i_in){}
+void internal_TC_K1_p(int& i_in){}
+void internal_T1_K1_t(int& i_in){}
+void internal_T2_K1_t(int& i_in){}
+void internal_T3_K1_t(int& i_in){}
+void internal_TC_K1_t(int& i_in){}
 
 
+void internal_T1_K2_a(int& i_in){}
+void internal_T2_K2_a(int& i_in){}
+void internal_T3_K2_a(int& i_in){}
+void internal_TC_K2_a(int& i_in){}
+void internal_T1_K2_p(int& i_in){}
+void internal_T2_K2_p(int& i_in){}
+void internal_T3_K2_p(int& i_in){}
+void internal_TC_K2_p(int& i_in){}
+void internal_T1_K2_t(int& i_in){}
+void internal_T2_K2_t(int& i_in){}
+void internal_T3_K2_t(int& i_in){}
+void internal_TC_K2_t(int& i_in){}
 
-
-auto internal_T1_K1_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K2_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K3_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K1_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K2_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K3_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K1_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K2_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K3_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K1_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K2_p(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K3_p(int i_in) -> int{
-    return i_in;
-}
-
-
-
-auto internal_T1_K1_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K2_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T1_K3_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K1_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K2_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T2_K3_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K1_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K2_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_T3_K3_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K1_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K2_t(int i_in) -> int{
-    return i_in;
-}
-auto internal_TC_K3_t(int i_in) -> int{
-    return i_in;
-}
+void internal_T1_K3_a(int& i_in){}
+void internal_T2_K3_a(int& i_in){}
+void internal_T3_K3_a(int& i_in){}
+void internal_TC_K3_a(int& i_in){}
+void internal_T1_K3_p(int& i_in){}
+void internal_T2_K3_p(int& i_in){}
+void internal_T3_K3_p(int& i_in){}
+void internal_TC_K3_p(int& i_in){}
+void internal_T1_K3_t(int& i_in){}
+void internal_T2_K3_t(int& i_in){}
+void internal_T3_K3_t(int& i_in){}
+void internal_TC_K3_t(int& i_in){}
 
 
 
