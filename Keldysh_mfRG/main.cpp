e249bf21dec0be6f22b061868f7318714ee48d41
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-19 16:33:10 +0100

    Included testSelfEnergy and sended sopt functions to the testFunctions.h file

--- Keldysh_mfRG/main.cpp
+++ Keldysh_mfRG/main.cpp
@@ -67,14 +67,14 @@ auto main() -> int {
     }
 #endif
 
-//    SelfEnergy<comp> self;
-//    for (int i = 0; i < nSE; ++i) {
-//        self.setself(0, i, U/2.);
-//        self.setself(1, i, 0.);
-//    }
-//    SelfEnergy<comp> zero;
-//    Propagator bubbles_prop = propag(1.0, self, zero, 'g');
-//    testBubbles(bubbles_prop, bubbles_prop);
+    State<comp> sopt_state;
+
+    setInitialConditions(sopt_state);
+
+    SelfEnergy<comp> zero;
+    Propagator bubbles_prop = propag(1.0, sopt_state.selfenergy, zero, 'g');
+    testBubbles(bubbles_prop, bubbles_prop);
+    testSelfEnergy(bubbles_prop, sopt_state);
 
     MPI_Finalize();
 
@@ -589,131 +589,4 @@ void writeOutFile(double Lambda, Propagator& propagator, SelfEnergy<comp>& selfE
 
     cout << "Wrote out. ";
     get_time(t_write);
-}
-
-
-template<typename Q>
-void sopt(State<Q>& dPsi, double Lambda, State<Q> &state) {
-
-    Propagator g = propag(Lambda, state.selfenergy, state.selfenergy, 'g');
-    cout << "G calculated" << endl;
-
-    bool diff = false;
-
-    cout << "bubble started" << endl;
-    double t2 = get_time();
-    //Lines 7-9
-    double ta = get_time();
-    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 'a', diff, '.');
-    cout<<  "a - Bubble:";
-    get_time(ta);
-
-    double tp = get_time();
-    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 'p', diff, '.');
-    cout<<  "p - Bubble:";
-    get_time(tp);
-
-    double tt = get_time();
-    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 't', diff, '.');
-    cout<<  "t - Bubble:";
-    get_time(tt);
-
-    cout << "bubble finished. ";
-    get_time(t2);
-
-    Propagator s = propag(Lambda, state.selfenergy, state.selfenergy, 's');
-
-    double tloop = get_time();
-    SelfEnergy<comp> Sigma_std = loop(dPsi.vertex, s);
-    cout << "loop calculated ";
-    get_time(tloop);
-
-    Sigma_std*= dL;
-
-    state.selfenergy += Sigma_std;
-
-}
-
-void writeOutSOPT(double Lambda, Propagator& propagator, SelfEnergy<comp>& selfEnergy, Vertex<fullvert<comp> >& vertex)
-{
-    int i = fconv_Lambda(Lambda);
-
-    ostringstream self_energyR, self_energyA, self_energyK, propR, propA, propK;
-    self_energyR << "Output/self_energyR"<<i<<".dat";
-    self_energyA << "Output/self_energyA"<<i<<".dat";
-    self_energyK << "Output/self_energyK"<<i<<".dat";
-
-    propR << "Output/propagatorR"<<i<<".dat";
-    propA << "Output/propagatorA"<<i<<".dat";
-    propK << "Output/propagatorK"<<i<<".dat";
-
-
-    ofstream my_file_sigmaR, my_file_sigmaA, my_file_sigmaK, my_file_propR, my_file_propA, my_file_propK;
-    my_file_sigmaR.open(self_energyR.str());
-    my_file_sigmaA.open(self_energyA.str());
-    my_file_sigmaK.open(self_energyK.str());
-
-    my_file_propR.open(propR.str());
-    my_file_propA.open(propA.str());
-    my_file_propK.open(propK.str());
-
-
-    for (int j = 0; j < ffreqs.size(); j++) {
-        my_file_sigmaR << ffreqs[j] << " " << selfEnergy.sval(0, j).real() << " " <<  selfEnergy.sval(0, j).imag() << "\n";
-        my_file_sigmaA << ffreqs[j] << " " << selfEnergy.sval(0, j).real() << " " << -selfEnergy.sval(0, j).imag() << "\n";
-        my_file_sigmaK << ffreqs[j] << " " << selfEnergy.sval(1, j).real() << " " <<  selfEnergy.sval(1, j).imag() << "\n";
-
-        my_file_propR << ffreqs[j] << " " << propagator.pval(0, j).real() << " " <<  propagator.pval(0, j).imag() << "\n";
-        my_file_propA << ffreqs[j] << " " << propagator.pval(0, j).real() << " " << -propagator.pval(0, j).imag() << "\n";
-        my_file_propK << ffreqs[j] << " " << propagator.pval(1, j).real() << " " <<  propagator.pval(1, j).imag() << "\n";
-    }
-
-#if DIAG_CLASS >=1
-    ostringstream avert1, pvert1, tvert1, avert3, pvert5, tvert3;
-
-    avert1 << "Output/avert1"<<i<<".dat";
-    pvert1 << "Output/pvert1"<<i<<".dat";
-    tvert1 << "Output/tvert1"<<i<<".dat";
-
-    avert3 << "Output/avert3"<<i<<".dat";
-    pvert5 << "Output/pvert5"<<i<<".dat";
-    tvert3 << "Output/tvert3"<<i<<".dat";
-
-    ofstream  my_file_avert1, my_file_pvert1, my_file_tvert1, my_file_avert3, my_file_pvert5, my_file_tvert3;
-
-    my_file_avert1.open(avert1.str());
-    my_file_pvert1.open(pvert1.str());
-    my_file_tvert1.open(tvert1.str());
-
-    my_file_avert3.open(avert3.str());
-    my_file_pvert5.open(pvert5.str());
-    my_file_tvert3.open(tvert3.str());
-
-    for (int j = 0; j<bfreqs.size(); j++){
-        my_file_avert1 << bfreqs[j] << " " << vertex.densvertex.avertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.avertex.K1_vval(0, j, 0).imag()<< "\n";
-        my_file_pvert1 << bfreqs[j] << " " << vertex.densvertex.pvertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.pvertex.K1_vval(0, j, 0).imag()<< "\n";
-        my_file_tvert1 << bfreqs[j] << " " << vertex.densvertex.tvertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.tvertex.K1_vval(0, j, 0).imag()<< "\n";
-
-        my_file_avert3 << bfreqs[j] << " " << vertex.densvertex.avertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.avertex.K1_vval(1, j, 0).imag()<< "\n";
-        my_file_pvert5 << bfreqs[j] << " " << vertex.densvertex.pvertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.pvertex.K1_vval(1, j, 0).imag()<< "\n";
-        my_file_tvert3 << bfreqs[j] << " " << vertex.densvertex.tvertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.tvertex.K1_vval(1, j, 0).imag()<< "\n";
-
-    }
-
-    my_file_avert1.close();
-    my_file_pvert1.close();
-    my_file_tvert1.close();
-
-    my_file_avert3.close();
-    my_file_pvert5.close();
-    my_file_tvert3.close();
-#endif
-
-    my_file_sigmaR.close();
-    my_file_sigmaA.close();
-    my_file_sigmaK.close();
-
-    my_file_propR.close();
-    my_file_propA.close();
-    my_file_propK.close();
 }
