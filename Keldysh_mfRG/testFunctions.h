c4a5d9beb6f2313af266f7bb2e81cdfec539cd0a
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-19 16:31:13 +0100

    Refined testFunctions

--- Keldysh_mfRG/testFunctions.h
+++ Keldysh_mfRG/testFunctions.h
@@ -2,74 +2,195 @@
 // Created by Sa.Aguirre on 2/19/20.
 //
 
+#include "loop.h"
+
 #ifndef KELDYSH_MFRG_TESTFUNCTIONS_H
 #define KELDYSH_MFRG_TESTFUNCTIONS_H
 
+template<typename Q>
+void sopt(State<Q>& dPsi, double Lambda, State<Q> &state) {
+
+    Propagator g = propag(Lambda, state.selfenergy, state.selfenergy, 'g');
+    cout << "G calculated" << endl;
+
+    bool diff = false;
+
+    cout << "bubble started" << endl;
+    double t2 = get_time();
+    //Lines 7-9
+    double ta = get_time();
+    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 'a', diff, '.');
+    cout<<  "a - Bubble:";
+    get_time(ta);
+
+    double tp = get_time();
+    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 'p', diff, '.');
+    cout<<  "p - Bubble:";
+    get_time(tp);
+
+    double tt = get_time();
+    bubble_function(dPsi.vertex, state.vertex, state.vertex, g, g, 't', diff, '.');
+    cout<<  "t - Bubble:";
+    get_time(tt);
+
+    cout << "bubble finished. ";
+    get_time(t2);
+
+
+    SelfEnergy<comp> Sigma_std = loop(dPsi.vertex, g);
+    cout << "loop calculated ";
+
+    state.selfenergy += Sigma_std;
+
+}
+
+void writeOutSOPT(double Lambda, Propagator& propagator, SelfEnergy<comp>& selfEnergy, Vertex<fullvert<comp> >& vertex)
+{
+    int i = fconv_Lambda(Lambda);
+
+    ostringstream self_energyR, self_energyA, self_energyK, propR, propA, propK;
+    self_energyR << "Output/self_energyR"<<i<<".dat";
+    self_energyA << "Output/self_energyA"<<i<<".dat";
+    self_energyK << "Output/self_energyK"<<i<<".dat";
+
+    propR << "Output/propagatorR"<<i<<".dat";
+    propA << "Output/propagatorA"<<i<<".dat";
+    propK << "Output/propagatorK"<<i<<".dat";
+
+
+    ofstream my_file_sigmaR, my_file_sigmaA, my_file_sigmaK, my_file_propR, my_file_propA, my_file_propK;
+    my_file_sigmaR.open(self_energyR.str());
+    my_file_sigmaA.open(self_energyA.str());
+    my_file_sigmaK.open(self_energyK.str());
+
+    my_file_propR.open(propR.str());
+    my_file_propA.open(propA.str());
+    my_file_propK.open(propK.str());
+
+
+    for (int j = 0; j < ffreqs.size(); j++) {
+        my_file_sigmaR << ffreqs[j] << " " << selfEnergy.sval(0, j).real() << " " <<  selfEnergy.sval(0, j).imag() << "\n";
+        my_file_sigmaA << ffreqs[j] << " " << selfEnergy.sval(0, j).real() << " " << -selfEnergy.sval(0, j).imag() << "\n";
+        my_file_sigmaK << ffreqs[j] << " " << selfEnergy.sval(1, j).real() << " " <<  selfEnergy.sval(1, j).imag() << "\n";
+
+        my_file_propR << ffreqs[j] << " " << propagator.pval(0, j).real() << " " <<  propagator.pval(0, j).imag() << "\n";
+        my_file_propA << ffreqs[j] << " " << propagator.pval(0, j).real() << " " << -propagator.pval(0, j).imag() << "\n";
+        my_file_propK << ffreqs[j] << " " << propagator.pval(1, j).real() << " " <<  propagator.pval(1, j).imag() << "\n";
+    }
+
+#if DIAG_CLASS >=1
+    ostringstream avert1, pvert1, tvert1, avert3, pvert5, tvert3;
+
+    avert1 << "Output/avert1"<<i<<".dat";
+    pvert1 << "Output/pvert1"<<i<<".dat";
+    tvert1 << "Output/tvert1"<<i<<".dat";
+
+    avert3 << "Output/avert3"<<i<<".dat";
+    pvert5 << "Output/pvert5"<<i<<".dat";
+    tvert3 << "Output/tvert3"<<i<<".dat";
+
+    ofstream  my_file_avert1, my_file_pvert1, my_file_tvert1, my_file_avert3, my_file_pvert5, my_file_tvert3;
+
+    my_file_avert1.open(avert1.str());
+    my_file_pvert1.open(pvert1.str());
+    my_file_tvert1.open(tvert1.str());
+
+    my_file_avert3.open(avert3.str());
+    my_file_pvert5.open(pvert5.str());
+    my_file_tvert3.open(tvert3.str());
+
+    for (int j = 0; j<bfreqs.size(); j++){
+        my_file_avert1 << bfreqs[j] << " " << vertex.densvertex.avertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.avertex.K1_vval(0, j, 0).imag()<< "\n";
+        my_file_pvert1 << bfreqs[j] << " " << vertex.densvertex.pvertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.pvertex.K1_vval(0, j, 0).imag()<< "\n";
+        my_file_tvert1 << bfreqs[j] << " " << vertex.densvertex.tvertex.K1_vval(0, j, 0).real() << " " << vertex.densvertex.tvertex.K1_vval(0, j, 0).imag()<< "\n";
+
+        my_file_avert3 << bfreqs[j] << " " << vertex.densvertex.avertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.avertex.K1_vval(1, j, 0).imag()<< "\n";
+        my_file_pvert5 << bfreqs[j] << " " << vertex.densvertex.pvertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.pvertex.K1_vval(1, j, 0).imag()<< "\n";
+        my_file_tvert3 << bfreqs[j] << " " << vertex.densvertex.tvertex.K1_vval(1, j, 0).real() << " " << vertex.densvertex.tvertex.K1_vval(1, j, 0).imag()<< "\n";
+
+    }
+
+    my_file_avert1.close();
+    my_file_pvert1.close();
+    my_file_tvert1.close();
+
+    my_file_avert3.close();
+    my_file_pvert5.close();
+    my_file_tvert3.close();
+#endif
+
+    my_file_sigmaR.close();
+    my_file_sigmaA.close();
+    my_file_sigmaK.close();
+
+    my_file_propR.close();
+    my_file_propA.close();
+    my_file_propK.close();
+}
+
 void testBubbles(Propagator& g1, Propagator& g2){
+    vector<comp> w (nBOS);
+    vector<comp> Pia_odd_even (nBOS);
+    vector<comp> Pia_odd_odd (nBOS);
 
-    vector<comp> Pia9  (nBOS); vector<comp> Pip9  (nBOS); vector<comp> Pit9  (nBOS);
-    vector<comp> Pia12 (nBOS); vector<comp> Pip12 (nBOS); vector<comp> Pit12 (nBOS);
-    vector<comp> Pia13 (nBOS); vector<comp> Pip13 (nBOS); vector<comp> Pit13 (nBOS);
-    vector<comp> Pia14 (nBOS); vector<comp> Pip14 (nBOS); vector<comp> Pit14 (nBOS);
-    vector<comp> Pia15 (nBOS); vector<comp> Pip15 (nBOS); vector<comp> Pit15 (nBOS);
-
-
-    int i = 0;
     for(auto w:bfreqs){
-        IntegrandBubble integrandPia9  (g1, g2, false, w, 9,  'a'); IntegrandBubble integrandPip9  (g1, g2, false, w, 9,  'p'); IntegrandBubble integrandPit9  (g1, g2, false, w, 9,  't');
-        IntegrandBubble integrandPia12 (g1, g2, false, w, 12, 'a'); IntegrandBubble integrandPip12 (g1, g2, false, w, 12, 'p'); IntegrandBubble integrandPit12 (g1, g2, false, w, 12, 't');
-        IntegrandBubble integrandPia13 (g1, g2, false, w, 13, 'a'); IntegrandBubble integrandPip13 (g1, g2, false, w, 13, 'p'); IntegrandBubble integrandPit13 (g1, g2, false, w, 13, 't');
-        IntegrandBubble integrandPia14 (g1, g2, false, w, 14, 'a'); IntegrandBubble integrandPip14 (g1, g2, false, w, 14, 'p'); IntegrandBubble integrandPit14 (g1, g2, false, w, 14, 't');
-        IntegrandBubble integrandPia15 (g1, g2, false, w, 15, 'a'); IntegrandBubble integrandPip15 (g1, g2, false, w, 15, 'p'); IntegrandBubble integrandPit15 (g1, g2, false, w, 15, 't');
+        int i = fconv_bos(w);
+        IntegrandBubble integrandPia6  (g1, g2, false, w, 6,  'a');
+        IntegrandBubble integrandPia9  (g1, g2, false, w, 9,  'a');
+        IntegrandBubble integrandPia11 (g1, g2, false, w, 11, 'a');
+        IntegrandBubble integrandPia13 (g1, g2, false, w, 13, 'a');
+        IntegrandBubble integrandPia15 (g1, g2, false, w, 15, 'a');
 
-
-        Pia9 [i] = integrator(integrandPia9 , w_lower_b, w_upper_b); Pip9 [i] = integrator(integrandPip9 , w_lower_b, w_upper_b); Pit9 [i] = integrator(integrandPit9 , w_lower_b, w_upper_b);
-        Pia12[i] = integrator(integrandPia12, w_lower_b, w_upper_b); Pip12[i] = integrator(integrandPip12, w_lower_b, w_upper_b); Pit12[i] = integrator(integrandPit12, w_lower_b, w_upper_b);
-        Pia13[i] = integrator(integrandPia13, w_lower_b, w_upper_b); Pip13[i] = integrator(integrandPip13, w_lower_b, w_upper_b); Pit13[i] = integrator(integrandPit13, w_lower_b, w_upper_b);
-        Pia14[i] = integrator(integrandPia14, w_lower_b, w_upper_b); Pip14[i] = integrator(integrandPip14, w_lower_b, w_upper_b); Pit14[i] = integrator(integrandPit14, w_lower_b, w_upper_b);
-        Pia15[i] = integrator(integrandPia15, w_lower_b, w_upper_b); Pip15[i] = integrator(integrandPip15, w_lower_b, w_upper_b); Pit15[i] = integrator(integrandPit15, w_lower_b, w_upper_b);
-        i++;
+        Pia_odd_even[i] = integrator(integrandPia11 , w_lower_b, w_upper_b) + integrator(integrandPia13 , w_lower_b, w_upper_b);
+        Pia_odd_odd [i] = integrator(integrandPia6 , w_lower_b, w_upper_b) + integrator(integrandPia9 , w_lower_b, w_upper_b) + integrator(integrandPia15 , w_lower_b, w_upper_b);
     }
 
 
-    ostringstream Pia9file;     ostringstream Pip9file;     ostringstream Pit9file;
-    ostringstream Pia12file;    ostringstream Pip12file;    ostringstream Pit12file;
-    ostringstream Pia13file;    ostringstream Pip13file;    ostringstream Pit13file;
-    ostringstream Pia14file;    ostringstream Pip14file;    ostringstream Pit14file;
-    ostringstream Pia15file;    ostringstream Pip15file;    ostringstream Pit15file;
+    ostringstream Pia_odd_evenfile;
+    ostringstream Pia_odd_oddfile;
 
-    Pia9file << "Output/Pia9.dat";      Pip9file << "Output/Pip9.dat";   Pit9file << "Output/Pit9.dat";
-    Pia12file << "Output/Pia12.dat";    Pip12file << "Output/Pip12.dat"; Pit12file << "Output/Pit12.dat";
-    Pia13file << "Output/Pia13.dat";    Pip13file << "Output/Pip13.dat"; Pit13file << "Output/Pit13.dat";
-    Pia14file << "Output/Pia14.dat";    Pip14file << "Output/Pip14.dat"; Pit14file << "Output/Pit14.dat";
-    Pia15file << "Output/Pia15.dat";    Pip15file << "Output/Pip15.dat"; Pit15file << "Output/Pit15.dat";
+    Pia_odd_evenfile << "Output/Pia_odd_even.dat";
+    Pia_odd_oddfile  << "Output/Pia_odd_odd.dat";
 
 
-    ofstream my_file_Pia9 ; ofstream my_file_Pip9 ; ofstream my_file_Pit9 ;
-    ofstream my_file_Pia12; ofstream my_file_Pip12; ofstream my_file_Pit12;
-    ofstream my_file_Pia13; ofstream my_file_Pip13; ofstream my_file_Pit13;
-    ofstream my_file_Pia14; ofstream my_file_Pip14; ofstream my_file_Pit14;
-    ofstream my_file_Pia15; ofstream my_file_Pip15; ofstream my_file_Pit15;
+    ofstream my_file_Pia_odd_even ;
+    ofstream my_file_Pia_odd_odd;
 
-    my_file_Pia9.open( Pia9file.str()) ; my_file_Pip9.open( Pip9file.str()) ; my_file_Pit9.open( Pit9file.str()) ;
-    my_file_Pia12.open(Pia12file.str()); my_file_Pip12.open(Pip12file.str()); my_file_Pit12.open(Pit12file.str());
-    my_file_Pia13.open(Pia13file.str()); my_file_Pip13.open(Pip13file.str()); my_file_Pit13.open(Pit13file.str());
-    my_file_Pia14.open(Pia14file.str()); my_file_Pip14.open(Pip14file.str()); my_file_Pit14.open(Pit14file.str());
-    my_file_Pia15.open(Pia15file.str()); my_file_Pip15.open(Pip15file.str()); my_file_Pit15.open(Pit15file.str());
+    my_file_Pia_odd_even.open(Pia_odd_evenfile.str()) ;
+    my_file_Pia_odd_odd.open( Pia_odd_oddfile.str());
 
     for(int i = 0; i<nBOS; i++){
-        my_file_Pia9  << bfreqs[i] << " " << Pia9 [i].real() << " " << Pia9 [i].imag() << "\n"; my_file_Pip9  << bfreqs[i] << " " << Pip9 [i].real() << " " << Pip9 [i].imag() << "\n"; my_file_Pit9  << bfreqs[i] << " " << Pit9 [i].real() << " " << Pit9 [i].imag() << "\n";
-        my_file_Pia12 << bfreqs[i] << " " << Pia12[i].real() << " " << Pia12[i].imag() << "\n"; my_file_Pip12 << bfreqs[i] << " " << Pip12[i].real() << " " << Pip12[i].imag() << "\n"; my_file_Pit12 << bfreqs[i] << " " << Pit12[i].real() << " " << Pit12[i].imag() << "\n";
-        my_file_Pia13 << bfreqs[i] << " " << Pia13[i].real() << " " << Pia13[i].imag() << "\n"; my_file_Pip13 << bfreqs[i] << " " << Pip13[i].real() << " " << Pip13[i].imag() << "\n"; my_file_Pit13 << bfreqs[i] << " " << Pit13[i].real() << " " << Pit13[i].imag() << "\n";
-        my_file_Pia14 << bfreqs[i] << " " << Pia14[i].real() << " " << Pia14[i].imag() << "\n"; my_file_Pip14 << bfreqs[i] << " " << Pip14[i].real() << " " << Pip14[i].imag() << "\n"; my_file_Pit14 << bfreqs[i] << " " << Pit14[i].real() << " " << Pit14[i].imag() << "\n";
-        my_file_Pia15 << bfreqs[i] << " " << Pia15[i].real() << " " << Pia15[i].imag() << "\n"; my_file_Pip15 << bfreqs[i] << " " << Pip15[i].real() << " " << Pip15[i].imag() << "\n"; my_file_Pit15 << bfreqs[i] << " " << Pit15[i].real() << " " << Pit15[i].imag() << "\n";
+        my_file_Pia_odd_even<< bfreqs[i] << " " << Pia_odd_even[i].real() << " " << Pia_odd_even [i].imag() << "\n";
+        my_file_Pia_odd_odd << bfreqs[i] << " " << Pia_odd_odd[i].real()  << " " << Pia_odd_odd[i].imag() << "\n";
     }
 
-    my_file_Pia9.close() ; my_file_Pip9.close() ; my_file_Pit9.close() ;
-    my_file_Pia12.close(); my_file_Pip12.close(); my_file_Pit12.close();
-    my_file_Pia13.close(); my_file_Pip13.close(); my_file_Pit13.close();
-    my_file_Pia14.close(); my_file_Pip14.close(); my_file_Pit14.close();
-    my_file_Pia15.close(); my_file_Pip15.close(); my_file_Pit15.close();
+    my_file_Pia_odd_even.close() ;
+    my_file_Pia_odd_odd.close();
+}
+
+void testSelfEnergy(Propagator& g1, State<comp>& state){
+
+    sopt(state, 1.0, state);
+
+    ostringstream selfEnergyR_cxx;
+    ostringstream selfEnergyK_cxx;
+
+    selfEnergyR_cxx << "Output/SelfEnergyR_cxx.dat";
+    selfEnergyK_cxx << "Output/SelfEnergyK_cxx.dat";
+
+    ofstream my_file_SelfEnergyR_cxx;
+    ofstream my_file_SelfEnergyK_cxx;
+
+    my_file_SelfEnergyR_cxx.open(selfEnergyR_cxx.str());
+    my_file_SelfEnergyK_cxx.open(selfEnergyK_cxx.str());
+
+    for(int i = 0; i<nFER; i++){
+        my_file_SelfEnergyR_cxx<< bfreqs[i] << " " << state.selfenergy.sval(0,i).real() << " " << state.selfenergy.sval(0,i).imag() << "\n";
+        my_file_SelfEnergyK_cxx<< bfreqs[i] << " " << state.selfenergy.sval(1,i).real() << " " << state.selfenergy.sval(1,i).imag() << "\n";
+    }
+
+    my_file_SelfEnergyR_cxx.close();
+    my_file_SelfEnergyK_cxx.close();
 }
 
 #endif //KELDYSH_MFRG_TESTFUNCTIONS_H
