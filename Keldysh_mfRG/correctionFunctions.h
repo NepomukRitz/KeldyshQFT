60a428d551783ff56d9d3ebf21be3c626d0ea82b
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-02-26 13:58:13 +0100

    w=0 was causing issues. Fixed them

--- Keldysh_mfRG/correctionFunctions.h
+++ Keldysh_mfRG/correctionFunctions.h
@@ -14,20 +14,31 @@
  * a and b define the propagators in question: for R => 1
  *                                             for A =>-1*/
 auto correctionFunctionBubbleAT(double w, double a, double b, double gamma_m, double gamma_p) -> comp{
-    return 1./(w+im_unit*GAMMA_REG*(b-a))
+    if(w==0.)
+        return 0.;
+    else
+        return 1./(w+im_unit*GAMMA_REG*(b-a))
             *(log((gamma_p+w/2.-epsilon+im_unit*GAMMA_REG*b)/(gamma_p-w/2.-epsilon+im_unit*GAMMA_REG*a))
             + log((gamma_m+w/2.+epsilon-im_unit*GAMMA_REG*a)/(gamma_m-w/2.+epsilon-im_unit*GAMMA_REG*b)));
 }
 
 auto correctionFunctionBubbleP(double w, double a, double b, double gamma_m, double gamma_p) -> comp{
-    return 1./(w-2*epsilon+im_unit*GAMMA_REG*(a+b))
+    if(w==2.*epsilon)
+        return 0.;
+    else
+        return 1./(w-2*epsilon+im_unit*GAMMA_REG*(a+b))
             *(log((gamma_p-w/2.+epsilon-im_unit*GAMMA_REG*b)/(gamma_p+w/2.-epsilon+im_unit*GAMMA_REG*a))
             + log((gamma_m-w/2.+epsilon-im_unit*GAMMA_REG*a)/(gamma_m+w/2.-epsilon+im_unit*GAMMA_REG*b)));
 }
 
 /*At this point, we work with corrections to bubbles of the kinds KR, KA, AK, RK, KK  being neglected, due to faster decay to zero (O(1/w^2))*/
 
-
+auto correctionFunctionSelfEnergy(double a, double gamma_m, double gamma_p) -> comp{
+    if(a==1.)
+        return log((gamma_m+epsilon-im_unit*GAMMA_REG*a)/(gamma_p-epsilon+im_unit*GAMMA_REG*a));
+    else
+        return 2.*im_unit*(atan((gamma_m+epsilon)/GAMMA_REG) + atan((gamma_p-epsilon)/GAMMA_REG)-pi);
+}
 
 
 #endif //KELDYSH_MFRG_CORRECTIONFUNCTIONS_H
