67e8df1a1ffd9d3fe285a5c38ed5be77c5047340
Fabian.Kugler <fabian.kugler@physik.uni-muenchen.de>
2020-03-09 13:26:21 +0100

    assignment operator in data structures and deleted parts which were commented out

--- Keldysh_mfRG/data_structures.h
+++ Keldysh_mfRG/data_structures.h
@@ -20,14 +20,15 @@ typedef complex<double> comp;
 // general vector class, defining element-wise addition, subtraction and multiplication
 template <typename T>
 class basic_vec : public vector<T> {
-  public:
-    basic_vec() : vector<T> () {};
-    basic_vec(int n) : vector<T> (n) {};
-    basic_vec(int n, T& value) : vector<T> (n, value) {}; // TODO: test
-    basic_vec(initializer_list<T> m) : vector<T> (m) {};
+public:
+    basic_vec() : vector<T> () {}; 						// trivial constructor
+    basic_vec(int n) : vector<T> (n) {};				// constructor with number of elements
+    basic_vec(int n, T value) : vector<T> (n, value) {};// constructor with number of elements and value
+    basic_vec(initializer_list<T> m) : vector<T> (m) {};// constructor from initializer lis
 
-    T& operator() (int i) {return (*this)[i]; }
+    T& operator() (int i) {return (*this)[i]; }			// operator for element access //TODO: T& (with reference) on purpose?
 
+    basic_vec<T>& operator= (const basic_vec<T> &m);	// element-wise assignment
     basic_vec<T> operator+  (const basic_vec<T> &m);    // element-wise addition of two vectors
     basic_vec<T> operator+  (const T &c);               // addition of a constant
     basic_vec<T> operator+= (const basic_vec<T> &m);    // element-wise addition of two vectors
@@ -38,10 +39,10 @@ class basic_vec : public vector<T> {
     basic_vec<T> operator-= (const T &c);               // subtraction of a constant
     basic_vec<T> operator*  (const basic_vec<T> &m);    // element-wise multiplication of two vectors
     basic_vec<T> operator*  (const T &c);               // multiplication with a constant
-    basic_vec<T> operator*  (double alpha);             // multiplication with a constant
+    basic_vec<T> operator*  (double alpha);             // multiplication with a double constant
     basic_vec<T> operator*= (const basic_vec<T> &m);    // element-wise multiplication of two vectors
     basic_vec<T> operator*= (const T &c);               // multiplication with a constant
-    basic_vec<T> operator*= (double alpha);             // multiplication with a constant
+    basic_vec<T> operator*= (double alpha);             // multiplication with a double constant
 
 };
 
@@ -49,22 +50,15 @@ class basic_vec : public vector<T> {
 // derived general vector class (e.g. for real vector: vec<double>)
 template <typename T>
 class vec : public basic_vec<T> {
-  public:
-    vec() : basic_vec<T> () {};
+public:
+    vec() : basic_vec<T> () {};							// constructors, see above
     vec(int n) : basic_vec<T> (n) {};
-    vec(int n, T& value) : basic_vec<T> (n, value) {}; // TODO: test
+    vec(int n, T value) : basic_vec<T> (n, value) {};
     vec(initializer_list<T> m) : basic_vec<T> (m) {};
 
-    vec<T> inv(); // element-wise inverse
+    using basic_vec<T>::operator=;		// use assignment operator from base class
 
-//    vec<T> operator+ (const vec<T> &m2);    //element-wise addition of two vectors
-//    vec<T> operator+ (const T &c);          // addition of a constant
-//    vec<T> operator- (const vec<T> &m2);    // element-wise subtraction of two vectors
-//    vec<T> operator- (const T &c);          // subtraction of a constant
-//    vec<T> operator- ();                    // invert sign
-//    vec<T> operator* (const vec<T> &m2);    // element-wise multiplication of two vectors
-//    vec<T> operator* (const T &c);          // multiplication with a constant
-//    vec<T> operator* (double c);            // multiplication with a double constant
+    vec<T> inv(); 										// element-wise inverse
 };
 
 
@@ -72,68 +66,19 @@ class vec : public basic_vec<T> {
 // inverse, real/imaginary part, absolute value, complex conjugate
 template <>
 class vec<comp> : public basic_vec<comp> {
-  public:
-    vec() : basic_vec<comp> () {};
+public:
+    vec() : basic_vec<comp> () {};						// constructors, see above
     explicit vec(int n) : basic_vec<comp> (n) {};
-    vec(int n, comp& value) : basic_vec<comp> (n, value) {}; // TODO: test
+    vec(int n, comp value) : basic_vec<comp> (n, value) {};
     vec(initializer_list<comp> m) : basic_vec<comp> (m) {};
 
-    //vec<comp> inv();    // element-wise inverse
-    //vec<double> real(); // element-wise real part
-    //vec<double> imag(); // element-wise imaginary part
-    //vec<double> abs();  // element-wise absolute value
-    //vec<comp> conj();   // element-wise complex conjugate
+    using basic_vec<comp>::operator=;		// use assignment operator from base class
 
-    // element-wise inverse
-    vec<comp> inv() {
-        vec<comp> temp (this->size());
-#pragma omp parallel for
-        for (int i=0; i<this->size(); ++i) {
-            temp[i] = 1./(*this)[i];
-        }
-        return temp;
-    }
-
-    // element-wise real part
-    vec<double> real() {
-        vec<double> temp (this->size());
-#pragma omp parallel for
-        for (int i=0; i<this->size(); ++i) {
-            temp[i] = (*this)[i].real();
-        }
-        return temp;
-    }
-
-    // element-wise imaginary part
-    vec<double> imag() {
-        vec<double> temp (this->size());
-#pragma omp parallel for
-        for (int i=0; i<this->size(); ++i) {
-            temp[i] = (*this)[i].imag();
-        }
-        return temp;
-    }
-
-    // element-wise absolute value
-    vec<double> abs() {
-        vec<double> temp (this->size());
-#pragma omp parallel for
-        for (int i=0; i<this->size(); ++i) {
-            temp[i] = std::abs((*this)[i]);
-        }
-        return temp;
-    }
-
-    // element-wise complex conjugate
-    vec<comp> conj() {
-        vec<comp> temp (this->size());
-#pragma omp parallel for
-        for (int i=0; i<this->size(); ++i) {
-            temp[i] = std::conj((*this)[i]);
-        }
-        return temp;
-    }
-
+    vec<comp> inv();    // element-wise inverse
+    vec<double> real(); // element-wise real part
+    vec<double> imag(); // element-wise imaginary part
+    vec<double> abs();  // element-wise absolute value
+    vec<comp> conj();   // element-wise complex conjugate
 };
 
 
@@ -151,6 +96,16 @@ typedef vec<comp> cvec;
 /// DEFINITIONS ///
 
 /* member functions of general vector class basic_vec<T> */
+
+// element-wise assignment
+template <typename T>
+basic_vec<T>& basic_vec<T>::operator=(const basic_vec<T> &m) {
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        (*this)[i] = m[i];
+    }
+    return *this;
+}
 
 // element-wise addition of two vectors
 template <typename T>
@@ -316,154 +271,55 @@ vec<T> vec<T>::inv() {
 
 /* member functions of derived complex vector class vec<comp> */
 
-//// element-wise inverse
-//vec<comp> vec<comp>::inv() {
-//  vec<comp> temp (this->size());
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i) {
-//    temp[i] = 1./(*this)[i];
-//  }
-//  return temp;
-//}
-//
-//// element-wise real part
-//vec<double> vec<comp>::real() {
-//  vec<double> temp (this->size());
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i) {
-//    temp[i] = (*this)[i].real();
-//  }
-//  return temp;
-//}
-//
-//// element-wise imaginary part
-//vec<double> vec<comp>::imag() {
-//  vec<double> temp (this->size());
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i) {
-//    temp[i] = (*this)[i].imag();
-//  }
-//  return temp;
-//}
-//
-//// element-wise absolute value
-//vec<double> vec<comp>::abs() {
-//  vec<double> temp (this->size());
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i) {
-//    temp[i] = std::abs((*this)[i]);
-//  }
-//  return temp;
-//}
-//
-//// element-wise complex conjugate
-//vec<comp> vec<comp>::conj() {
-//  vec<comp> temp (this->size());
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i) {
-//    temp[i] = std::conj((*this)[i]);
-//  }
-//  return temp;
-//}
+// element-wise inverse
+vec<comp> vec<comp>::inv() {
+    vec<comp> temp (this->size());
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        temp[i] = 1./(*this)[i];
+    }
+    return temp;
+}
 
+// element-wise real part
+vec<double> vec<comp>::real() {
+    vec<double> temp (this->size());
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        temp[i] = (*this)[i].real();
+    }
+    return temp;
+}
 
-//
-//// element-wise addition of two vectors
-//template <typename T>
-//vec<T> vec<T>:: operator+ (const vec<T> &m2) {
-//#pragma omp parallel for
-//  for (int i=0; i<this->size(); ++i){
-//      (*this)[i] + m2[i];
-//  }
-//  return *this;
-//}
-//
-//// addition of a constant
-//template <typename T>
-//vec<T> operator+ (const vec<T> &m, const T &c) {
-//  vec<T> temp (m.size());
-//#pragma omp parallel for
-//  for (int i=0; i<m.size(); ++i){
-//    temp[i] = m[i] + c;
-//  }
-//  return temp;
-//}
-//
-//// element-wise subtraction of two vectors
-//template <typename T>
-//vec<T> operator- (const vec<T> &m1, const vec<T> &m2) {
-//  vec<T> temp (m1.size());
-//#pragma omp parallel for
-//  for (int i=0; i<m1.size(); ++i){
-//    temp[i] = m1[i] - m2[i];
-//  }
-//  return temp;
-//}
-//
-//// subtraction of a constant
-//template <typename T>
-//vec<T> operator- (const T &c) {
-//#pragma omp parallel for
-//  for (int i=0; i<m.size(); ++i){
-//    (*this)[i] = m[i] - c;
-//  }
-//  return temp;
-//}
-//
-//// invert sign
-//template <typename T>
-//vec<T> operator- (const vec<T> &m) {
-//  vec<T> temp (m.size());
-//#pragma omp parallel for
-//  for (int i=0; i<m.size(); ++i){
-//    temp[i] = - m[i];
-//  }
-//  return temp;
-//}
-//
-//// element-wise multiplication of two vectors
-//template <typename T>
-//vec<T> operator* (const vec<T> &m1, const vec<T> &m2) {
-//  vec<T> temp (m1.size());
-//#pragma omp parallel for
-//  for (int i=0; i<m1.size(); ++i){
-//    temp[i] = m1[i] * m2[i];
-//  }
-//  return temp;
-//}
-//
-//// multiplication with a constant
-//template <typename T>
-//vec<T> operator* (const vec<T> &m, const T &c) {
-//  vec<T> temp (m.size());
-//#pragma omp parallel for
-//  for (int i=0; i<m.size(); ++i){
-//    temp[i] = m[i] * c;
-//  }
-//  return temp;
-//}
-//
-//// multiplication with a constant
-//template <typename T>
-//vec<T> operator* (const vec<T> &m, double c) {
-//    vec<T> temp (m.size());
-//#pragma omp parallel for
-//    for (int i=0; i<m.size(); ++i){
-//        temp[i] = m[i] * c;
-//    }
-//    return temp;
-//}
-//
-//template <typename T>
-//vec<T> operator* (double c, const vec<T> &m) {
-//    vec<T> temp (m.size());
-//#pragma omp parallel for
-//    for (int i=0; i<m.size(); ++i){
-//        temp[i] = m[i] * c;
-//    }
-//    return temp;
-//}
+// element-wise imaginary part
+vec<double> vec<comp>::imag() {
+    vec<double> temp (this->size());
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        temp[i] = (*this)[i].imag();
+    }
+    return temp;
+}
 
+// element-wise absolute value
+vec<double> vec<comp>::abs() {
+    vec<double> temp (this->size());
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        temp[i] = std::abs((*this)[i]);
+    }
+    return temp;
+}
+
+// element-wise complex conjugate
+vec<comp> vec<comp>::conj() {
+    vec<comp> temp (this->size());
+#pragma omp parallel for
+    for (int i=0; i<this->size(); ++i) {
+        temp[i] = std::conj((*this)[i]);
+    }
+    return temp;
+}
 
 #endif // DATA_STRUCTURES_H
 
