e5bf2fd388e099fe88969778079a03a779198572
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-03-11 19:37:40 +0100

    Added export_data functions for visualization of the flow

--- Keldysh_mfRG/solvers.h
+++ Keldysh_mfRG/solvers.h
@@ -6,6 +6,48 @@
 #define KELDYSH_MFRG_SOLVERS_H
 
 #include <cmath> // needed for exponential and sqrt function
+#include "write_data2file.h"
+#include <string>
+
+
+template <typename T>
+void export_data(T& state, int iter){}
+
+/**
+ * Exports data of a state (i.e. Retarded and Keldysh SelfEnergies plus values stored in K1-class (for SOPT, these values are the bubbles)
+ * @param state     : State<comp> whose data is going to be printed
+ * @param iter      : Number of the iteration
+ */
+void export_data(State<comp>& state, int iter){
+
+    //Names for the different keys
+    string name = "SOPT_flowstep_" + to_string(iter) + ".h5";
+    string ReSER = "SOPT_ReSER";
+    string ImSER = "SOPT_ImSER";
+    string ReSEK = "SOPT_ReSEK";
+    string ImSEK = "SOPT_ImSEK";
+    string RePiaOE = "SOPT_RePiaOE";
+    string ImPiaOE = "SOPT_ImPiaOE";
+    string RePiaOO = "SOPT_RePiaOO";
+    string ImPiaOO = "SOPT_ImPiaOO";
+
+    //Prealocation of buffer for data and successive allocation
+    cvec PiaOE(nBOS);
+    cvec PiaOO(nBOS);
+    cvec SER(nBOS);
+    cvec SEK(nBOS);
+    for (int j = 0; j < nBOS; j++) {
+        PiaOE[iter] = 4. * state.vertex.spinvertex.avertex.K1_vval(0, iter, 0);
+        PiaOO[iter] = 4. * state.vertex.spinvertex.avertex.K1_vval(1, iter, 0);
+        SER[iter] = state.selfenergy.val(0, iter);
+        SEK[iter] = state.selfenergy.val(1, iter);
+    }
+
+    //Write out to file with name "name"
+    write_h5_rvecs(name, {"w", ReSER, ImSER, ReSEK, ImSEK, RePiaOE, ImPiaOE, RePiaOO, ImPiaOO},
+                   {bfreqs, SER.real(), SER.imag(), SEK.real(), SEK.imag(), PiaOE.real(), PiaOE.imag(),
+                    PiaOO.real(), PiaOO.imag()});
+}
 
 template <typename T>
 void ODE_solver_Euler(T& y_fin, const double x_fin, const T& y_ini, const double x_ini, T rhs (const T& y, const double x), const int N_ODE) {
@@ -15,6 +57,7 @@ void ODE_solver_Euler(T& y_fin, const double x_fin, const T& y_ini, const double
     for (int i=0; i<N_ODE; ++i) {
         x_run += dx; // update x
         y_run += rhs(y_run, x_run) * dx; // update y
+        export_data(y_run, i+1);
     }
     y_fin = y_run; // final y value
 }
@@ -31,137 +74,51 @@ void ODE_solver_RK4(T& y_fin, const double x_fin, const T& y_ini, const double x
         T y3 = rhs(y_run + y2/2., x_run + dx/2.) * dx;
         T y4 = rhs(y_run + y3, x_run + dx) * dx;
         y_run += (y1 + 2.*y2 + 2.*y3 + y4) / 6.; // update y
+        export_data(y_run, i+1);
     }
     y_fin = y_run; // final y value
 }
 
-//State<comp> test_rhs_state(const State<comp>& Psi, const double Lambda) {
-//
-//    State<comp> dPsi;
-//
-///*Here I begin implementing Fabian's pseudocode*/
-//    //Line 1
-//    Propagator S(Lambda, Psi.selfenergy, 's');
-//    print("S calculated", true);
-//    //Line 2
-//    Propagator G(Lambda, Psi.selfenergy, 'g');
-//    print("G calculated", true);
-//    //Line 3
-//    SelfEnergy<comp> Sigma_std;
-//    loop(Sigma_std, Psi.vertex, S);
-//    print("loop calculated", true);
-//    //Line 4
-//    dPsi.selfenergy = Sigma_std;
-//    //Line 6
-//    Propagator dG (Lambda, Psi.selfenergy, dPsi.selfenergy, 'k');
-//
-//
-//
-//    print("diff bubble started", true);
-//    bool diff = true;
-//    double t2 = get_time();
-//    //Lines 7-9
-//    double ta = get_time();
-//    bubble_function(dPsi.vertex, state.vertex, state.vertex, G, dG, 'a', diff, '.');
-//    print("a - Bubble:");
-//    get_time(ta);
-//
-//    double tp = get_time();
-//    bubble_function(dPsi.vertex, state.vertex, state.vertex, G, dG, 'p', diff, '.');
-//    print("p - Bubble:");
-//    get_time(tp);
-//
-//    double tt = get_time();
-//    bubble_function(dPsi.vertex, state.vertex, state.vertex, G, dG, 't', diff, '.');
-//    print("t - Bubble:");
-//    get_time(tt);
-//
-//    print("diff bubble finished. ");
-//    get_time(t2);
-//
-//#ifdef SOPT
-//    Propagator s = propag(Lambda, state.selfenergy, state.selfenergy, 's');
-//    loop(dPsi.selfenergy, state.vertex, s);
-//#endif
-//
-//#ifdef NLOOPS
-//    if(nLoops>1) {
-//        //    Lines 10-13   => Multi-loop
-//        double t4 = get_time();
-//        /*Create two new vertices to accommodate the contributions on each side */
-//        Vertex<fullvert<Q> > dGammaL;
-//        Vertex<fullvert<Q> > dGammaR;
-//        //Change from differentiated to regular bubbles
-//        diff = false;
-//
-//        bubble_function(dGammaL, dPsi.vertex, state.vertex, G, G, 'a', diff, 'L');
-//        bubble_function(dGammaL, dPsi.vertex, state.vertex, G, G, 'p', diff, 'L');
-//        bubble_function(dGammaL, dPsi.vertex, state.vertex, G, G, 't', diff, 'L');
-//
-//        bubble_function(dGammaR, state.vertex, dPsi.vertex, G, G, 'a', diff, 'R');
-//        bubble_function(dGammaR, state.vertex, dPsi.vertex, G, G, 'p', diff, 'R');
-//        bubble_function(dGammaR, state.vertex, dPsi.vertex, G, G, 't', diff, 'R');
-//
-//        print("Bubbles calculated: ", true);
-//        get_time(t4);
-//
-//
-//        //Lines 14-17
-//        Vertex<fullvert<Q> > dGammaT = dGammaL + dGammaR;
-//        dPsi.vertex += dGammaT;
-//        print("2-loops done. \n");
-//
-//
-//        //Lines 18-33
-//        if (nLoops >= 3) {
-//            Vertex<fullvert<Q> > dGammaC;
-//            Vertex<fullvert<Q> > dGammaCtb;
-//            for (int i = 3; i <= nLoops; i++) {
-//                bubble_function(dGammaC, state.vertex, dGammaL, G, G, 'a', diff, 'C');
-//                bubble_function(dGammaC, state.vertex, dGammaL, G, G, 'p', diff, 'C');
-//
-//                //This corresponds to Line 29 in the pseudo-code and is important for self-energy corrections.
-//                dGammaCtb += dGammaC;
-//
-//                bubble_function(dGammaC, state.vertex, dGammaL, G, G, 't', diff, 'C');
-//
-//                bubble_function(dGammaL, dGammaT, state.vertex, G, G, 'a', diff, 'L');
-//                bubble_function(dGammaL, dGammaT, state.vertex, G, G, 'p', diff, 'L');
-//                bubble_function(dGammaL, dGammaT, state.vertex, G, G, 't', diff, 'L');
-//
-//                bubble_function(dGammaR, state.vertex, dGammaT, G, G, 'a', diff, 'R');
-//                bubble_function(dGammaR, state.vertex, dGammaT, G, G, 'p', diff, 'R');
-//                bubble_function(dGammaR, state.vertex, dGammaT, G, G, 't', diff, 'R');
-//
-//                dGammaT = dGammaL + dGammaC + dGammaR;
-//                dPsi.vertex += dGammaT;
-//
-////        if(max_r(norm(dGammaT)/norm(dPsi.vertex)) < tol_vertex){ //TODO define a sensible norm for the vertices and a good way to implement this condition
-////            break;
-////        }
-//                printf("%i-loops done. \n", i);
-//            }
-//        }
-//    }
-//
-//#endif
-//
-////#if PROP_TYPE==2
-////    //Lines 33-41
-////    SelfEnergy<comp> dSigma_tbar;
-////    loop(dSigma_tbar, dGammaCtb, G);
-////    Propagator corrected = propag(Lambda, dPsi.selfenergy, dSigma_tbar, 'e', false);
-////    SelfEnergy<comp> dSigma_t;
-////    loop(dSigma_t, dPsi.vertex, corrected);
-////    dPsi.selfenergy += (dSigma_t + dSigma_tbar);
-////#endif
-//
-//
-//    double t_multiply = get_time();
-//    dPsi *= dL;
-//    print("dPsi multiplied. ");
-//    get_time(t_multiply);
-//}
+auto test_rhs_state(const State<comp>& Psi, const double Lambda) -> State<comp> {
+
+    State<comp> dPsi;
+
+    //Line 1
+    Propagator S(Lambda, Psi.selfenergy, 's');
+    //Line 2
+    Propagator G(Lambda, Psi.selfenergy, 'g');
+
+    print("diff bubble started", true);
+    bool diff = true;
+    double t2 = get_time();
+    //Lines 7-9
+    double ta = get_time();
+    bubble_function(dPsi.vertex, Psi.vertex, Psi.vertex, G, S, 'a', diff, '.');
+    print("a - Bubble:");
+    get_time(ta);
+
+    double tp = get_time();
+    bubble_function(dPsi.vertex, Psi.vertex, Psi.vertex, G, S, 'p', diff, '.');
+    print("p - Bubble:");
+    get_time(tp);
+
+    double tt = get_time();
+    bubble_function(dPsi.vertex, Psi.vertex, Psi.vertex, G, S, 't', diff, '.');
+    print("t - Bubble:");
+    get_time(tt);
+
+    print("diff bubble finished. ");
+    get_time(t2);
+
+    loop(dPsi.selfenergy, Psi.vertex, S);
+
+    double t_multiply = get_time();
+    dPsi *= dL;
+    print("dPsi multiplied. ");
+    get_time(t_multiply);
+
+    return dPsi;
+}
 
 double test_rhs_ODE_exp(const double& y, const double x) {
     return y;
