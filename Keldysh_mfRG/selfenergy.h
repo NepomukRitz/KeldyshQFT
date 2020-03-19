628691a3b4de50187c2442e53a2c3af0670ea1d5
Fabian.Kugler <fabian.kugler@physik.uni-muenchen.de>
2020-03-19 14:32:48 +0100

    cosmetics in self-energy class

--- Keldysh_mfRG/selfenergy.h
+++ Keldysh_mfRG/selfenergy.h
@@ -11,13 +11,13 @@ class SelfEnergy{
     vec<Q> Sigma = vec<Q> (2*nSE*n_in); // factor 2 for Keldysh components: Sigma^R, Sigma^K
 public:
     // TODO: comment member functions
-    void initialize(Q, Q);
-    auto val(int, int, int) const -> Q;
-    auto valsmooth(int, double, int) const -> Q;
-    void setself(int, int, int, Q);
-    void addself(int, int, int, Q);
-    auto acc(int) -> Q;// access to the ith element of the vector "SIGMA"
-    void direct_set(int, Q);
+    void initialize(Q valR, Q valK);
+    auto val(int iK, int iv, int i_in) const -> Q;
+    auto valsmooth(int iK, double v, int i_in) const -> Q;
+    void setself(int iK, int iv, int i_in, Q val);
+    void addself(int iK, int iv, int i_in, Q val);
+    auto acc(int i) -> Q;// access to the ith element of the vector "Sigma"
+    void direct_set(int i, Q val);
 
     // operators for self energy
     auto operator+= (const SelfEnergy<Q>& self1) -> SelfEnergy<Q> {//sum operator overloading
@@ -69,53 +69,53 @@ template <typename Q> void SelfEnergy<Q>::initialize(Q valR, Q valK) {
     }
 }
 
-template <typename Q> auto SelfEnergy<Q>::val(int iK, int i, int i_in) const -> Q{
-    return Sigma[iK*nSE*n_in + i*n_in + i_in];
+template <typename Q> auto SelfEnergy<Q>::val(int iK, int iv, int i_in) const -> Q{
+    return Sigma[iK*nSE*n_in + iv*n_in + i_in];
 }
 
 template <typename Q> auto SelfEnergy<Q>::acc(int i) -> Q{
     if(i>=0 && i < Sigma.size()){
     return Sigma[i];}
-    else{cout << "Error: Tried to access value outside of self energy range" << endl;};
+    else{cout << "Error: Tried to access value outside of self-energy range." << endl;};
 }
 
-template <typename Q> void SelfEnergy<Q>::direct_set(int i, Q value) {
+template <typename Q> void SelfEnergy<Q>::direct_set(int i, Q val) {
     if(i>=0 && i < Sigma.size()){
-    Sigma[i] = value;}
-    else{cout << "Error: Tried to access value outside of self energy range" << endl;};
+    Sigma[i] = val;}
+    else{cout << "Error: Tried to access value outside of self-energy range." << endl;};
 }
 
-template <typename Q> auto SelfEnergy<Q>::valsmooth(int iK, double w, int i_in) const -> Q {//smoothly interpolates for values between discrete frequency values of mesh
+template <typename Q> auto SelfEnergy<Q>::valsmooth(int iK, double v, int i_in) const -> Q {//smoothly interpolates for values between discrete frequency values of mesh
 
-    if(fabs(w)>w_upper_b)
-        //Returns U/2 for Retarded and 0. for Keldysh
+    if(abs(v)>w_upper_f)
+        //Returns U/2 for retarded and 0. for Keldysh component
         return (1.-(double)iK)*glb_U/2.;
     else {
-        if(fabs(w)!= w_upper_f) {
-            int W = fconv_fer(w);
-            double x1 = ffreqs[W];
-            double x2 = ffreqs[W + 1];
-            double xd = (w - x1) / (x2 - x1);
+        if(fabs(v)!= w_upper_f) { // linear interpolation
+            int iv = fconv_fer(v); // index corresponding to v
+            double x1 = ffreqs[iv]; // lower adjacent frequency value
+            double x2 = ffreqs[iv + 1]; // upper adjacent frequency value
+            double xd = (v - x1) / (x2 - x1); // distance between adjacent frequnecy values
 
-            Q f1 = val(iK, W, i_in);
-            Q f2 = val(iK, W + 1, i_in);
+            Q f1 = val(iK, iv, i_in); // lower adjacent value
+            Q f2 = val(iK, iv + 1, i_in);  // upper adjacent value
 
-            return (1. - xd) * f1 + xd * f2;
+            return (1. - xd) * f1 + xd * f2; // interpolated value
         }
-        else if(w == w_upper_f)
+        else if(v == w_upper_f)
             return val(iK, nSE-1, i_in);
-        else if(w == w_lower_f)
+        else if(v == w_lower_f)
             return val(iK, 0, i_in);
     }
 
 }
 
-template <typename Q> void SelfEnergy<Q>::setself(int iK, int i, int i_in, Q val){
-    Sigma[iK*nSE + i*n_in + i_in] = val;
+template <typename Q> void SelfEnergy<Q>::setself(int iK, int iv, int i_in, Q val){
+    Sigma[iK*nSE + iv*n_in + i_in] = val;
 }
 
-template <typename Q> void SelfEnergy<Q>::addself(int iK, int i, int i_in, Q val){
-    Sigma[iK*nSE + i*n_in + i_in] += val;
+template <typename Q> void SelfEnergy<Q>::addself(int iK, int iv, int i_in, Q val){
+    Sigma[iK*nSE + iv*n_in + i_in] += val;
 }
 
 
