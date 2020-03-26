cfb726ba8d99cd41417d89346d1179ede97334f1
Sa.Aguirre <Sa.Aguirre@physik.uni-muenchen.de>
2020-03-26 12:30:09 +0100

    Commented entire Class

--- Keldysh_mfRG/selfenergy.h
+++ Keldysh_mfRG/selfenergy.h
@@ -10,14 +10,14 @@ class SelfEnergy{
 public:
     // TODO: split into two members: Sigma_R, Sigma_K (?)
     vec<Q> Sigma = vec<Q> (2*nSE*n_in); // factor 2 for Keldysh components: Sigma^R, Sigma^K
-    // TODO: comment member functions
-    void initialize(Q valR, Q valK);
-    auto val(int iK, int iv, int i_in) const -> Q;
-    auto valsmooth(int iK, double v, int i_in) const -> Q;
-    void setself(int iK, int iv, int i_in, Q val);
-    void addself(int iK, int iv, int i_in, Q val);
-    auto acc(int i) -> Q;// access to the ith element of the vector "Sigma"
-    void direct_set(int i, Q val);
+
+    void initialize(Q valR, Q valK);    //Initializes SE to given values
+    auto val(int iK, int iv, int i_in) const -> Q;  //Returns value at given input on freq grid
+    auto valsmooth(int iK, double v, int i_in) const -> Q;  //Returns interpolated value at given input
+    void setself(int iK, int iv, int i_in, Q val);  //Sets value of SE at given input
+    void addself(int iK, int iv, int i_in, Q val);  //Adds given input to specified location
+    auto acc(int i) -> Q;// access to the ith element of the vector "Sigma" (hdf5-relevant)
+    void direct_set(int i, Q val);  //Direct set value to i-th location (hdf5-relevant)
 
     // operators for self energy
     auto operator+= (const SelfEnergy<Q>& self1) -> SelfEnergy<Q> {//sum operator overloading
@@ -58,6 +58,12 @@ public:
 
 
 /*****************************************FUNCTIONS FOR SELF ENERGY*****************************************************/
+/**
+ * Function initializes the Retarded and Keldysh components of the Self Energy
+ * @tparam Q    : Type of values (usually comp)
+ * @param valR  : Value for the constant Retarded Self Energy
+ * @param valK  : Value for the constant Keldyh Self Energy
+ */
 template <typename Q> void SelfEnergy<Q>::initialize(Q valR, Q valK) {
     //Assign self energy to initial values
 #pragma omp parallel for
@@ -69,27 +75,55 @@ template <typename Q> void SelfEnergy<Q>::initialize(Q valR, Q valK) {
     }
 }
 
+/**
+ * Returns the saved value of the Self Energy
+ * @tparam Q    : Type of values (usually comp)
+ * @param iK    : Keldysh index (either 0 or 1)
+ * @param iv    : Frequency index
+ * @param i_in  : Internal structure index
+ * @return The value of SigmaR/K at the chosen indices
+ */
 template <typename Q> auto SelfEnergy<Q>::val(int iK, int iv, int i_in) const -> Q{
     return Sigma[iK*nSE*n_in + iv*n_in + i_in];
 }
 
+/**
+ * Access function to element i (hdf5-relevant)
+ * @tparam Q : Type of values (usually comp)
+ * @param i  : Index
+ * @return Sigma[i]
+ */
 template <typename Q> auto SelfEnergy<Q>::acc(int i) -> Q{
     if(i>=0 && i < Sigma.size()){
     return Sigma[i];}
     else{cout << "Error: Tried to access value outside of self-energy range." << endl;};
 }
 
+/**
+ * Set value of Self Energy at index i
+ * @tparam Q : Type of values (usually comp)
+ * @param i  : Index
+ * @param val: Value
+ */
 template <typename Q> void SelfEnergy<Q>::direct_set(int i, Q val) {
     if(i>=0 && i < Sigma.size()){
     Sigma[i] = val;}
     else{cout << "Error: Tried to access value outside of self-energy range." << endl;};
 }
 
+/**
+ * Linear interpolating function returning the value of the Retarded or Keldysh Self Energy at freq v
+ * @tparam Q    : Type of values (usually comp)
+ * @param iK    : Keldysh index (either 0 or 1)
+ * @param v     : Frequency
+ * @param i_in  : Internal index
+ * @return      : Interpolated value using the saved values as basis
+ */
 template <typename Q> auto SelfEnergy<Q>::valsmooth(int iK, double v, int i_in) const -> Q {//smoothly interpolates for values between discrete frequency values of mesh
 
-    if(abs(v)>w_upper_f)
+    if(abs(v)>w_upper_f)    //Check the range of frequency. If too large, return Sigma(\infty)
         //Returns U/2 for retarded and 0. for Keldysh component
-        return (1.-(double)iK)*glb_U/2.;
+        return (1.-(double)iK)*glb_U/2.;    //TODO: Generalize this to include differential Self Energies
     else {
         if(fabs(v)!= w_upper_f) { // linear interpolation
             int iv = fconv_fer(v); // index corresponding to v
@@ -102,18 +136,34 @@ template <typename Q> auto SelfEnergy<Q>::valsmooth(int iK, double v, int i_in) 
 
             return (1. - xd) * f1 + xd * f2; // interpolated value
         }
-        else if(v == w_upper_f)
+        else if(v == w_upper_f) //Exactly at the upper boundary
             return val(iK, nSE-1, i_in);
-        else if(v == w_lower_f)
+        else if(v == w_lower_f) //Exactly at the lower boundary
             return val(iK, 0, i_in);
     }
 
 }
 
+/**
+ * Sets the value of the Self Energy to a specific value at the specified location
+ * @tparam Q    : Type of values (usually comp)
+ * @param iK    : Keldysh index (either 0 or 1)
+ * @param iv    : Frequency index
+ * @param i_in  : Internal index
+ * @param val   : Value of the Self Energy at this given point
+ */
 template <typename Q> void SelfEnergy<Q>::setself(int iK, int iv, int i_in, Q val){
     Sigma[iK*nSE + iv*n_in + i_in] = val;
 }
 
+/**
+ * Adds the given value at the specified location
+ * @tparam Q    : Type of values (usually comp)
+ * @param iK    : Keldysh index (either 0 or 1)
+ * @param iv    : Frequency index
+ * @param i_in  : Internal index
+ * @param val   : Value of the Self Energy at this given point
+ */
 template <typename Q> void SelfEnergy<Q>::addself(int iK, int iv, int i_in, Q val){
     Sigma[iK*nSE + iv*n_in + i_in] += val;
 }
