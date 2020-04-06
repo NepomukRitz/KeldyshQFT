b52e47a999670b5c353d1d561412a0421895b073
E.Walter <e.walter@physik.uni-muenchen.de>
2020-04-06 16:01:21 +0200

    compile script lrz

--- Keldysh_mfRG/compile_lrz.sh
+++ Keldysh_mfRG/compile_lrz.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 module load hdf5/serial
-module load fftw
+module load fftw/mpi/v3_mkl2019
 
 export LANG=C
 export LC_ALL=C
 
-mpiCC --std=c++11  main.cpp -o main.o -fopenmp $HDF5_INC $HDF5_CPP_LIB $HDF5_LIB $FFTW_INC $FFTW_LIB $SZIP_LIB -lz
+HDF5="$HDF5_INC $HDF5_CPP_LIB $HDF5_LIB $SZIP_LIB -lz"
+FFTW="$FFTW_INC $FFTW_LIB"
+
+mpiCC --std=c++11  main.cpp -o main.o -fopenmp $HDF5 $FFTW
+
