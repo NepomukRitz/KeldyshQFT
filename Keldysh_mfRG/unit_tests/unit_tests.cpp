ad42ed925b51655ee0724852d03c703577ecf9d4
E.Walter <e.walter@physik.uni-muenchen.de>
2020-08-24 19:44:12 +0200

    add new CMake executable for unit tests

--- Keldysh_mfRG/unit_tests/unit_tests.cpp
+++ Keldysh_mfRG/unit_tests/unit_tests.cpp
@@ -2,30 +2,6 @@
  * Main file for performing unit tests using Catch.
  */
 
-#include <iostream>          // text input/output
-#include "data_structures.h" // real/complex vector classes
-#include "write_data2file.h" // writing data into text or hdf5 files
-#include "parameters.h"
-#include <mpi.h>
-#include "mpi_setup.h"
-#include "solvers.h"
-#include "frequency_grid.h"
-#include "util.h"
-#include "selfenergy.h"
-#include "propagator.h"
-#include "Keldysh_symmetries.h"
-#include "fourier_trafo.h" // Fourier transforms in physics convention and SOPT using FFT
-#include "right_hand_sides.h"
-#include "vertex.h"
-#include "state.h"
-#include "loop.h"
-#include "bubbles.h"
-#include "testFunctions.h"
-#include "hdf5_routines.h"
-#include "flow.h"
-#include "tests/omp_test.h"
-
-
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
