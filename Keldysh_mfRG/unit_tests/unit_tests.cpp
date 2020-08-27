d4a283a38d8fe6bd4736330d83da9c1e9a723b6b
E.Walter <e.walter@physik.uni-muenchen.de>
2020-08-27 17:53:50 +0200

    update structure of unit_tests.cpp (MPI necessary since bubbles.h only works with MPI)

--- Keldysh_mfRG/unit_tests/unit_tests.cpp
+++ Keldysh_mfRG/unit_tests/unit_tests.cpp
@@ -1,6 +1,5 @@
 /**
  * Main file for performing unit tests using Catch.
- * Contains a predefined main() function defined in catch.hpp (used if INTEGRATION_TESTS is not defined).
  * Unit tests can be added by including a header file test_<file>.h, in which test cases for <file>.h are defined.
  * Macro INTEGRATION_TESTS determines if integration tests specified in the main() function below should be run.
  */
@@ -8,15 +7,10 @@
 // if defined, also run integration tests (else only unit tests)
 #define INTEGRATION_TESTS
 
-#ifndef INTEGRATION_TESTS
-    #define CATCH_CONFIG_MAIN       // use main() function from catch.hpp
-#else
-    #define CATCH_CONFIG_RUNNER     // define main() function with integration tests
-#endif
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
 #include "../parameters.h"  // define system parameters
-#undef MPI_FLAG             // make sure that MPI is not used during unit testing
 
 // include tests that should be run
 #include "test_data_structures.h"
@@ -25,12 +19,21 @@
 
 #ifdef INTEGRATION_TESTS
 #include "../testFunctions.h"
+#endif
 
 int main(int argc, char* argv[]) {
+#ifdef MPI_FLAG
+    MPI_Init(nullptr, nullptr);
+#endif
 
-    // integration tests come here
+#ifdef INTEGRATION_TESTS
+    // run integration tests
+#endif
+
+#ifdef MPI_FLAG
+    MPI_Finalize();
+#endif
 
     // run unit tests
     return Catch::Session().run(argc, argv);
 }
-#endif
