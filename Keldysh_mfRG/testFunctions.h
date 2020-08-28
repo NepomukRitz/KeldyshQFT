498a48aa3af20d5caf95280709fec0803ca64507
E.Walter <e.walter@physik.uni-muenchen.de>
2020-08-28 12:16:30 +0200

    added checks of causality for SE and FDTs for SE and K1 to testFunctions.h

--- Keldysh_mfRG/testFunctions.h
+++ Keldysh_mfRG/testFunctions.h
@@ -12,7 +12,67 @@
 #include "perturbation_theory.h"
 
 
+/**
+ * Function that checks causality of self-energy: Im(Sigma^R)<=0.
+ */
+template <typename Q>
+void check_SE_causality(SelfEnergy<Q> selfEnergy) {
+    print("Causality check of self-energy: Im(Sigma^R)<=0.", true);
 
+    vec<Q> Sigma = selfEnergy.Sigma;                        // take self-energy
+    vec<Q> Sigma_R (&Sigma[0], &Sigma[Sigma.size()/2]);     // take first half of self-energy (retarded comp.)
+
+    // check if Im(Sigma^R) is positive for every data point
+    int cnt = 0;
+    double sum = 0.;
+    for (int i=0; i<Sigma_R.size(); ++i) {
+        double val = Sigma_R[i].imag();
+        if (val > 0.) {
+            cnt += 1;
+            sum += val;
+        }
+    }
+    if (cnt > 0) {
+        print("Selfenergy is non-causal: ", true);
+        print(cnt, " values of Im(Sigma^R) are positive, with a sum of ", sum, true);
+    } else
+        print("Selfenergy is causal.", true);
+}
+
+/**
+ * Function that checks FDTs for self-energy and K1 in all channels for given input state: Re(Sigma^K)=0, Re(K1r^K)=0.
+ */
+template <typename Q>
+void check_FDTs(State<Q>& state) {
+    print("Check of FDTs for self-energy and K1: Re(Sigma^K)=0, Re(K1r^K)=0.", true);
+
+    /** 1st check: real part of Keldysh component of the selfenergy has to be zero */
+
+    vec<Q> Sigma = state.selfenergy.Sigma;                          // take self-energy
+    vec<Q> Sigma_K (&Sigma[Sigma.size()/2], &Sigma[Sigma.size()]);  // take second half of self-energy (Keldysh comp.)
+    double max_Sigma_K = Sigma_K.real().max_norm();                 // take max. value of Re(Sigma^K)
+
+    print("Maximal value of Re(Sigma^K): ", max_Sigma_K, true);
+
+    /** 2nd check: real part of Keldysh component of K1 in all channels has to be zero */
+
+    // take K1 vertices in all channels
+    vec<Q> K1a = state.vertex[0].avertex.K1;
+    vec<Q> K1p = state.vertex[0].pvertex.K1;
+    vec<Q> K1t = state.vertex[0].tvertex.K1;
+    // take second half of K1 vertices (Keldysh comp.)
+    vec<Q> K1a_K (&K1a[K1a.size()/2], &K1a[K1a.size()]);
+    vec<Q> K1p_K (&K1p[K1p.size()/2], &K1p[K1p.size()]);
+    vec<Q> K1t_K (&K1t[K1t.size()/2], &K1t[K1t.size()]);
+    // take max. value of Re(K1r^K)
+    double max_K1a_K = K1a_K.real().max_norm();
+    double max_K1p_K = K1p_K.real().max_norm();
+    double max_K1t_K = K1t_K.real().max_norm();
+
+    print("Maximal value of Re(K1a^K):   ", max_K1a_K, true);
+    print("Maximal value of Re(K1p^K):   ", max_K1p_K, true);
+    print("Maximal value of Re(K1t^K):   ", max_K1t_K, true);
+}
 
 /**
  * Function desgined to test the flow of the vertex in SOPT using the State class
