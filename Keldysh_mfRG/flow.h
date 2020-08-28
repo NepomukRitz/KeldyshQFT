4f36e6c7e9971de4360b760cef011415f665ecf0
E.Walter <e.walter@physik.uni-muenchen.de>
2020-08-28 12:21:22 +0200

    return final state of n_loop_flow to allow to run checks on it immediately without having to load the saved hdf5 file

--- Keldysh_mfRG/flow.h
+++ Keldysh_mfRG/flow.h
@@ -11,7 +11,7 @@
 #include "testFunctions.h"      // for initialization with SOPT at the beginning of the flow, using sopt_state // TODO: should this really be in testFunctions?
 #include "right_hand_sides.h"   // to compute right hand side of flow equation
 
-void n_loop_flow(string outputFileName){
+State<comp> n_loop_flow(string outputFileName){
 
     State<comp> state_fin, state_ini;   // create final and initial state
     state_ini.initialize();             // initialize state
@@ -21,10 +21,12 @@ void n_loop_flow(string outputFileName){
     write_hdf(outputFileName, 0, nODE + U_NRG.size() + 1, state_ini);  // save the initial state to hdf5 file
 
     // compute the flow using RK4 solver
-    ODE_solver_RK4(state_fin, Lambda_fin, state_ini, Lambda_ini, rhs_n_loop_flow,   // use one-loop-flow rhs
-                   sq_substitution, sq_resubstitution,                                       // use substitution for Lambda steps
+    ODE_solver_RK4(state_fin, Lambda_fin, state_ini, Lambda_ini, rhs_n_loop_flow,              // use n-loop-flow rhs
+                   sq_substitution, sq_resubstitution,                                             // use substitution for Lambda steps
                    nODE,
                    outputFileName);                                                                // save state at each step during flow
+
+    return state_fin;
 }
 
 
