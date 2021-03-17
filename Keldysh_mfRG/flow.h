f85b8f19517c582a5ed8ea2a0f695adddd662977
Elias Walter <e.walter@physik.uni-muenchen.de>
2021-03-17 16:48:43 +0100

    add overloads for n_loop_flow and ODE_solver to allow checkpointing: start at some intermediate iteration to continue previously cancelled job

--- Keldysh_mfRG/flow.h
+++ Keldysh_mfRG/flow.h
@@ -11,6 +11,10 @@
 #include "testFunctions.h"      // for initialization with SOPT at the beginning of the flow, using sopt_state // TODO: should this really be in testFunctions?
 #include "right_hand_sides.h"   // to compute right hand side of flow equation
 
+/**
+ * Compute n-loop flow, with number of loops specified by N_LOOPS in parameters.h.
+ * Initialize the flow with second order PT at Lambda_ini, compute the flow with RK4 ODE solver up to Lambda_fin.
+ */
 State<state_datatype> n_loop_flow(string outputFileName){
 
     State<state_datatype> state_fin (Lambda_fin), state_ini (Lambda_ini);   // create final and initial state
@@ -27,6 +31,35 @@ State<state_datatype> n_loop_flow(string outputFileName){
                    outputFileName);                                                                // save state at each step during flow
 
     return state_fin;
+}
+
+/**
+ * Checkpointing: Continue to compute an n-loop flow that has been canceled before, e.g. due to running into the wall
+ * time. For given iteration it_start, read the state at this iteration from previously computed results, then continue
+ * to compute the flow up to Lambda_fin.
+ *
+ * Usage: Check the number <Nmax> of the last Lambda layer of a given file <inputFileName> that has been successfully
+ *        computed. (See log file: "Successfully saved in hdf5 file: <inputFileName> in Lambda layer <Nmax>.)
+ *        Use this number <Nmax> as input <it_start> for this function.
+ */
+State<comp> n_loop_flow(string inputFileName, const int it_start) {
+    if (it_start < nODE + U_NRG.size() + 1) { // start iteration needs to be within the range of values
+
+        State<comp> state_ini = read_hdf(inputFileName, it_start, nODE + U_NRG.size() + 1); // read initial state
+        State<comp> state_fin (Lambda_fin);
+
+        // compute the flow using RK4 solver
+        ODE_solver_RK4(state_fin, Lambda_fin, state_ini, Lambda_ini, rhs_n_loop_flow,   // use one-loop-flow rhs
+                       sq_substitution, sq_resubstitution,      // use substitution for Lambda steps
+                       nODE,
+                       inputFileName,                           // save state at each step during flow
+                       it_start);                               // start from iteration it_start
+
+        return state_fin;
+    }
+    else {
+        print("Error: Start iteration is too large.", true);
+    }
 }
 
 
