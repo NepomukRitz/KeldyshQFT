a583009f4491d33518a8d89633bfb5a7de652678
Nepomuk Ritz <nepomuk.ritz@physik.uni-muenchen.de>
2021-08-11 09:29:16 +0200

    Removed unnecessary include directives from main.cpp.

--- Keldysh_mfRG/main.cpp
+++ Keldysh_mfRG/main.cpp
@@ -1,29 +1,11 @@
 #include <iostream>          // text input/output
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <bits/stdc++.h>
-#include "data_structures.h" // real/complex vector classes
-#include "write_data2file.h" // writing data into text or hdf5 files
 #include "parameters.h"
 #include <mpi.h>
 #include "mpi_setup.h"
 #include "testFunctions.h"
-#include "solvers.h"
-#include "frequency_grid.h"
 #include "util.h"
-#include "selfenergy.h"
-#include "propagator.h"
-#include "Keldysh_symmetries.h"
-#include "fourier_trafo.h" // Fourier transforms in physics convention and SOPT using FFT
-#include "right_hand_sides.h"
-#include "vertex.h"
-#include "state.h"
-#include "loop.h"
-#include "bubbles.h"
-#include "hdf5_routines.h"
-#include "flow.h"
-#include "tests/omp_test.h"
-#include <cassert>
 
 using namespace std;
 
