525ee51901072d26c46d8c67c8d97caec8890c43
Anxiang.Ge <anxiang.ge@physik.uni-muenchen.de>
2021-10-08 23:32:33 +0200

    added new option for printing the integrand using the results from the flow

--- Keldysh_mfRG/scripts/python_scripts/getIntegrand.py
+++ Keldysh_mfRG/scripts/python_scripts/getIntegrand.py
@@ -63,6 +63,29 @@ mpiCC --std=c++17  ../tests/get_integrand_dGamma_1loop.cpp -o ../tests/get_integ
     os.system("bash ./compile_asc_saveIntegrand.sh")
     os.system("rm ./compile_asc_saveIntegrand.sh")
 
+def compile_get_integrand_dGamma_1loop_fromFlow_Cpp():
+    text = """
+#!/bin/bash
+
+module load hdf5/1.10.5-gcc
+
+export LANG=C
+export LC_ALL=C
+
+MPI="-I/usr/include/mpi/"
+HDF5="-I/software/opt/bionic/x86_64/hdf5/1.10.5-gcc/include -L/software/opt/bionic/x86_64/hdf5/1.10.5-gcc/lib -lhdf5 -lhdf5_cpp"
+FFTW="-lfftw3"
+
+mpiCC --std=c++17  ../tests/get_integrand_dGamma_1loop_fromFlow.cpp -o ../tests/get_integrand_dGamma_1loop_fromFlow.o -fopenmp $MPI $HDF5 $FFTW
+
+    """
+    f = open("./compile_asc_saveIntegrand.sh", "w")
+    f.write(text)
+    f.close()
+    print("Compiling get_integrand_dGamma_1loop_fromFlow")
+    os.system("bash ./compile_asc_saveIntegrand.sh")
+    os.system("rm ./compile_asc_saveIntegrand.sh")
+
 
 def compile_get_integrand_dGammaL_Cpp():
     text = """
@@ -111,6 +134,10 @@ mpiCC --std=c++17  ../tests/get_integrand_dGammaC.cpp -o ../tests/get_integrand_
     os.system("bash ./compile_asc_saveIntegrand.sh")
     os.system("rm ./compile_asc_saveIntegrand.sh")
 
+
+def get_integrand_dGamma_1loop_fromFlow(exe_dir, data_dir, it_Lambda, rkStep, k_class_int, channel, i0, i2, w, v, vp, i_in):
+    cmd = exe_dir + 'get_integrand_dGamma_1loop_fromFlow.o ' + data_dir + " " + str(it_Lambda) + " " + str(rkStep) + " " + str(k_class_int) + " " + str(channel) + " " + str(i0) + " " + str(i2) + " {}".format(w) + " {}".format(v) + " {}".format(vp) + " " + str(i_in)
+    os.system(cmd)
 
 def get_integrand_dGamma_1loop(exe_dir, data_dir, it_Lambda, rkStep, k_class_int, channel, i0, i2, w, v, vp, i_in):
     cmd = exe_dir + 'get_integrand_dGamma_1loop.o ' + data_dir + " " + str(it_Lambda) + " " + str(rkStep) + " " + str(k_class_int) + " " + str(channel) + " " + str(i0) + " " + str(i2) + " {}".format(w) + " {}".format(v) + " {}".format(vp) + " " + str(i_in)
@@ -188,11 +215,21 @@ if __name__ == '__main__':
     channel = 'a'
     i0 = 0
     i2 = 0
-    w = 100
+    w = 0
     v = 0
     vp = 0
     i_in = 0
 
+
+    #compile_get_integrand_dGamma_1loop_fromFlow_Cpp()
+    get_integrand_dGamma_1loop(exe_dir, data_dir, it_Lambda, rkStep, k_class_int, channel, i0, i2, w, v, vp, i_in)
+
+    integrandFilename = get_integrand_filename_dGamma_1loop(data_dir, it_Lambda, rkStep, k_class_int, channel, i0, i2, w, v, vp, i_in)
+    fig0, axs0 = plt.subplots(num=0, nrows=1, ncols=1)
+    plot_integrand(axs0, integrandFilename, {"label": "testtest", "ls": "--"})
+    fig0.savefig(integrandFilename[:-3]+".png")
+
+    '''
     #compile_get_integrand_dGamma_1loop_Cpp()
     get_integrand_dGamma_1loop(exe_dir, data_dir, it_Lambda, rkStep, k_class_int, channel, i0, i2, w, v, vp, i_in)
 
@@ -223,6 +260,6 @@ if __name__ == '__main__':
     plot_integrand(axs3, integrandFilename, {"label": "testtest", "ls": "-"})
     fig3.savefig(integrandFilename[:-3]+".png")
 
-
+    '''
 
     #plt.show()
