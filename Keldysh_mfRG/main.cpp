e74183af77ca19ad1e300b57e7b5c18dfb4e0279
Nepomuk Ritz <nepomuk.ritz@physik.uni-muenchen.de>
2021-12-17 16:00:15 +0100

    Changed the file type of unit test files to let them be included only when testing.

--- Keldysh_mfRG/main.cpp
+++ Keldysh_mfRG/main.cpp
@@ -112,13 +112,13 @@ auto main() -> int {
 
     //
     //test_PT4(0.0, true);
-    test_PT_state<state_datatype>(data_dir+filename, 1.8, false);
+    //test_PT_state<state_datatype>(data_dir+filename, 1.8, false);
     //findBestWscale4K1<state_datatype>(1.8);
     //compute_non_symmetric_diags(0.8, true, 1, true);
     //test_integrate_over_K1<state_datatype>(1.8);
 
     std::string name = data_dir+filename+job;
-    //n_loop_flow(name, true);
+    n_loop_flow(name, true);
     //test_symmetries(1.8);
     //get_integrand_dGamma_1Loop<state_datatype>(data_dir, 1, 0);
 
