e2c52fe9d5fddc1335ca03f92396508e76287b61
Nepomuk Ritz <nepomuk.ritz@physik.uni-muenchen.de>
2022-02-03 13:18:23 +0100

    Started setting up compile options for JSC.

--- Keldysh_mfRG/scripts/compile_cluster.sh
+++ Keldysh_mfRG/scripts/compile_cluster.sh
@@ -7,6 +7,7 @@
 if [ "$1" == "--ASC" ]
   then
   export CLUSTER=ASC
+  module load cmake
 elif [ "$1" == "--KCS" ]
 then
   export CLUSTER=KCS
@@ -17,12 +18,19 @@ then
   module load fftw
   module load gsl
   module load boost/1.61_icc
+  module load cmake
+elif [ "$1" == "--JSC" ]
+then
+  export CLUSTER=JSC
+  module load Stages/2022 Intel/2021.4.0 OpenMPI/4.1.2
+  module load HDF5/1.12.1 # could be problematic, because already version 1.10 (instead of 1.08) and no threadsafe option
+  module load FFTW
+  module load GSL
+  module load CMake/3.21.1
 else
-  echo "Invalid cluster option! Needs to be --ASC or --KCS."
+  echo "Invalid cluster option! Needs to be --ASC, --KCS or --JSC"
   exit
 fi
-module load cmake
-
 
 export LANG=C
 export LC_ALL=C
