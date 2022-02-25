3d3eb4427ea6a2a40f2b4f863b02246831b53889
Nepomuk Ritz <ra49hif@kcs-login.cos.lrz.de>
2022-02-25 12:01:35 +0100

    Set up batchfile for SLURM system on KCS.

--- Keldysh_mfRG/scripts/batchfile.sh
+++ Keldysh_mfRG/scripts/batchfile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#SBATCH --job-name=jobname
+#SBATCH --job-name=SIAM_PT4
 #SBATCH --mem=2040
-#SBATCH --time=2-20:00:00
+#SBATCH --time=0-04:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=<username>@physik.uni-muenchen.de
-#SBATCH --chdir=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/<lrz-ID>/mfrg/Keldysh_mfRG/
-#SBATCH --output=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/<lrz-ID>/mfrg/Keldysh_mfRG/runs/jobname.%j.%N.out
-#SBATCH --error=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/<lrz-ID>/mfrg/Keldysh_mfRG/runs/jobname.%j.%N.err
-#SBATCH --nodes=4
+#SBATCH --mail-user=nepomuk.ritz@physik.uni-muenchen.de
+#SBATCH --chdir=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/ra49hif/mfrg/Keldysh_mfRG/
+#SBATCH --output=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/ra49hif/mfrg/Keldysh_mfRG/runs/jobname.%j.%N.out
+#SBATCH --error=/dss/dsskcsfs01/pn34vu/pn34vu-dss-0000/ra49hif/mfrg/Keldysh_mfRG/runs/jobname.%j.%N.err
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 ##SBATCH --cpus-per-task=32
 
@@ -17,4 +17,4 @@ echo $SLURM_ARRAY_JOB_ID
 echo $SLURM_NTASKS
 
 export OMP_NUM_THREADS=32
-mpiexec -n $SLURM_NTASKS ./main.o
+mpiexec -n $SLURM_NTASKS ./Keldysh_mfRG
