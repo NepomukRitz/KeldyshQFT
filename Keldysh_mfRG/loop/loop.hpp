cc1c931ac9c19cda374222e9c76c903c5b9f5b86
Anxiang.Ge <+HNw_FcAb>
2022-08-09 11:02:56 +0200

    fixed bug: in loop spin sum now performed in the integrandSE

--- Keldysh_mfRG/loop/loop.hpp
+++ Keldysh_mfRG/loop/loop.hpp
@@ -14,6 +14,7 @@
 #include "../utilities/write_data2file.hpp"        // save integrand for debugging purposes
 #include "../asymptotic_corrections/correction_functions.hpp"    // analytical results for the tails of the loop integral
 #include "integrandSE.hpp"
+#include "../utilities/hdf5_routines.hpp"
 
 /**
  * The selenergy loop can be closed in two different versions:
@@ -450,13 +451,13 @@ void LoopCalculator<Q,vertType,all_spins, version>::compute_Matsubara_zeroT() {
                                                                  {v}, Delta, true);
 
         // If taking spins sum, add contribution of all-spins-equal vertex: V -> 2*V + V^
-        if (all_spins) {
-            integratedR *= 2.;
-            IntegrandSE<Q,vertType,all_spins,Q,version> integrand_Vhat (0, fullvertex, prop, 0, pick_spin<version,1>(), v, i_in);
-            // split up the integrand at discontinuities and (possible) kinks:
-            integratedR += prefactor * integrator_Matsubara_T0(integrand_Vhat, v_lower-std::abs(v), v_upper+std::abs(v), 0.,
-                                                                    {v}, Delta, true);
-        }
+        //if (all_spins) {
+        //    integratedR *= 2.;
+        //    IntegrandSE<Q,vertType,all_spins,Q,version> integrand_Vhat (0, fullvertex, prop, 0, pick_spin<version,1>(), v, i_in);
+        //    // split up the integrand at discontinuities and (possible) kinks:
+        //    integratedR += prefactor * integrator_Matsubara_T0(integrand_Vhat, v_lower-std::abs(v), v_upper+std::abs(v), 0.,
+        //                                                            {v}, Delta, true);
+        //}
 
 
         //integratedR += -1./(2.*M_PI)
@@ -478,11 +479,11 @@ void LoopCalculator<Q,vertType,all_spins, version>::compute_Matsubara_finiteT() 
 
         integratedR = - glb_T * matsubarasum<Q>(integrand, Nmin, Nmax);
 
-        if (all_spins) {
-            integratedR *= 2.;
-            IntegrandSE<Q,vertType,all_spins,Q,version> integrand_Vhat (0, fullvertex, prop, 0, pick_spin<version,1>(), v, i_in);
-            integratedR += - glb_T * matsubarasum<Q>(integrand_Vhat, Nmin, Nmax);
-        }
+        //if (all_spins) {
+        //    integratedR *= 2.;
+        //    IntegrandSE<Q,vertType,all_spins,Q,version> integrand_Vhat (0, fullvertex, prop, 0, pick_spin<version,1>(), v, i_in);
+        //    integratedR += - glb_T * matsubarasum<Q>(integrand_Vhat, Nmin, Nmax);
+        //}
 
         /// in MF: use symmetric_full integration interval => asymptotic correction=0
         //integratedR += - 1./(2.*M_PI)
